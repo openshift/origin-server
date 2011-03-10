62b1f6c31eed7179b710785ee620a46bc1de2fa2
Dan McPherson <dmcphers@redhat.com>
2011-03-09 23:27:39 -0500

    site updates

--- site/app/controllers/users_controller.rb
+++ site/app/controllers/users_controller.rb
@@ -9,44 +9,46 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])    
-    unless @user.valid?
+    if !@user.valid?
       render :new
-    end
-
-    # Otherwise call out to IT's service to register
-    # Map any errors into the user.errors object
-
-    begin
-      url = URI.parse('https://streamline.devlab.phx1.redhat.com/wapps/streamline/registration.html')
-      req = Net::HTTP::Post.new(url.path)
-      
-      req.set_form_data({ 'emailAddress' => @user.emailAddress,  
-                          'password' => @user.password, 
-                          'passwordConfirmation' => @user.passwordConfirmation,
-                          'secretKey' => 'c0ldW1n3',
-                          'termsAccepted' => 'true'
-                          })
-      http = Net::HTTP.new(url.host, url.port)
-      if url.scheme == "https"
-        http.use_ssl = true
-        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-      end
-      response = http.start {|http| http.request(req)}
-      case response
-      when Net::HTTPSuccess, Net::HTTPRedirection
-        puts "HTTP response from server is:"
-        response.each do |k,v|
-            puts "#{k.to_s}: #{v.to_s}"
+    else
+      # Otherwise call out to IT's service to register
+      # Map any errors into the user.errors object
+  
+      begin
+        url = URI.parse('https://streamline.devlab.phx1.redhat.com/wapps/streamline/registration.html')
+        req = Net::HTTP::Post.new(url.path)
+        
+        req.set_form_data({ 'emailAddress' => @user.emailAddress,  
+                            'password' => @user.password, 
+                            'passwordConfirmation' => @user.passwordConfirmation,
+                            'secretKey' => 'c0ldW1n3',
+                            'termsAccepted' => 'true',
+                            'redirectUrl' => 'http://mcpherson.redhat.com:9292'
+                            })
+        http = Net::HTTP.new(url.host, url.port)
+        if url.scheme == "https"
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE
         end
-        puts response.body
-      else
-        puts "Problem with server. Response code was #{response.code}"
-        puts "HTTP response from server is #{response.body}"
-        response.error!
+        response = http.start {|http| http.request(req)}
+        case response
+        when Net::HTTPSuccess
+          puts "HTTP response from server is:"
+          response.each do |k,v|
+              puts "#{k.to_s}: #{v.to_s}"
+          end
+          puts response.body
+        else
+          puts "Problem with server. Response code was #{response.code}"
+          puts "HTTP response from server is #{response.body}"
+          response.error!
+        end
+  
+      rescue Net::HTTPBadResponse => e
+        puts e
+        raise
       end
-
-    rescue Net::HTTPBadResponse => e
-      puts e
     end
   end
 end
