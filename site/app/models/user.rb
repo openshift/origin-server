2ae765cce8ecc2f0c19bc8323052607745e29b33
Dan McPherson <dmcphers@redhat.com>
2011-03-15 16:45:18 -0400

    change password requirement to 6 chars

--- site/app/models/user.rb
+++ site/app/models/user.rb
@@ -5,7 +5,7 @@ class User
 
   attr_accessor :emailAddress, :password, :passwordConfirmation, :termsAccepted
   validates_format_of :emailAddress, :with => /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i, :message => 'Invalid email address'
-  validates_length_of :password, :minimum => 8, :message => 'Passwords must be at least 8 characters'  
+  validates_length_of :password, :minimum => 6, :message => 'Passwords must be at least 6 characters'  
   validates_each :termsAccepted do |record, attr, value|
     record.errors.add attr, 'Terms must be accepted' if value != '1'
   end
