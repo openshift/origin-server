b355ab11d3c4e021a04f5bafe7e41840df4cb549
Matt Hicks <mhicks@redhat.com>
2011-03-24 00:50:35 -0400

    Client tool gem packaging

--- server/config/application.rb
+++ server/config/application.rb
@@ -4,7 +4,6 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "rails/test_unit/railtie"
-require "libra"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
