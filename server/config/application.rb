6b0e334cef29292016ca0d7a1c3f211ca8b6824f
Matt Hicks <mhicks@redhat.com>
2011-03-23 20:49:23 -0400

    Restructuring directories to align with SRPM structure

--- server/config/application.rb
+++ server/config/application.rb
@@ -4,6 +4,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
 require "rails/test_unit/railtie"
+require "libra"
 
 # If you have a Gemfile, require the gems listed there, including any gems
 # you've limited to :test, :development, or :production.
