89da1758bee6ba5bc5636bcb5c9294c62b903e9f
Matt Hicks <mhicks@redhat.com>
2011-04-06 23:44:57 -0400

    Adding mock tests for web_user

--- server/test/test_helper.rb
+++ server/test/test_helper.rb
@@ -1,6 +1,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'mocha'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
@@ -36,3 +37,4 @@ class ActiveSupport::TestCase
     return result
   end
 end
+
