b980023e2318a82403e1a290068a97a6bb2c06fc
Dan McPherson <dmcphers@redhat.com>
2011-04-28 22:32:54 -0400

    dont default /user/new to express for forum users
    remove comment

--- server/app/controllers/user_controller.rb
+++ server/app/controllers/user_controller.rb
@@ -55,7 +55,7 @@ class UserController < ApplicationController
       when CloudAccess::FLEX
         action = 'confirm_flex'
         @product = 'flex'
-      else CloudAccess::EXPRESS # default to express
+      when CloudAccess::EXPRESS
         action = 'confirm_express'
         @product = 'express'
       end
