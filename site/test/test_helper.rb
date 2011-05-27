872a289cf287ff8b1a805a412cfa421f45e40db9
Dan McPherson <dmcphers@redhat.com>
2011-05-27 01:00:42 -0400

    fix site tests

--- site/test/test_helper.rb
+++ site/test/test_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha'
+$:.unshift(File.dirname(__FILE__) + '/../../server-common')
+require 'openshift'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
