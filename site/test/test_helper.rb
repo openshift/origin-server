0d93f92c7132d7a0b6414779a679979b001134fe
Dan McPherson <dmcphers@redhat.com>
2011-06-17 09:57:19 -0400

    get tests running again

--- site/test/test_helper.rb
+++ site/test/test_helper.rb
@@ -1,8 +1,8 @@
 ENV["RAILS_ENV"] = "test"
+$:.unshift(File.dirname(__FILE__) + '/../../server-common')
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha'
-$:.unshift(File.dirname(__FILE__) + '/../../server-common')
 require 'openshift'
 
 class ActiveSupport::TestCase
