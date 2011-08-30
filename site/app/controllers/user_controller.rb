d9d7dbacdd3711130440585e9b9de224d01a4fdc
Dan McPherson <dmcphers@redhat.com>
2011-08-29 20:39:20 -0400

    add external register

--- site/app/controllers/user_controller.rb
+++ site/app/controllers/user_controller.rb
@@ -5,6 +5,8 @@ require 'recaptcha'
 require 'json'
 
 class UserController < ApplicationController
+  
+  protect_from_forgery :except => :create_external
 
   def new(cloud_access_choice=nil)
     @product = 'openshift' unless defined? @product
@@ -77,6 +79,55 @@ class UserController < ApplicationController
     @event = 'event29'
     
     # Redirect to a running workflow if it exists
-    workflow_redirect    
+    workflow_redirect
+  end
+  
+  def create_json_error_hash(user_errors)
+    errors = {}
+    user_errors.keys.each do |key|
+      errors[key] = user_errors[key]
+    end
+    errors
+  end
+  
+  def create_external
+    Rails.logger.debug "External registration request"
+
+    data = JSON.parse(params[:json_data])
+      
+    @user = WebUser.new(data)
+      
+    registration_referrer = params[:registration_referrer]
+
+    # Run validations
+    if !@user.valid?
+      json = JSON.generate({:errors => create_json_error_hash(@user.errors)})
+      render :json => json, :status => :bad_request and return
+    end
+    
+    if params[:captcha_secret] != Rails.configuration.captcha_secret
+      render :nothing => true, :status => :unauthorized and return
+    end
+
+    begin
+      confirmationUrl = url_for(:action => 'confirm_external',
+                                :controller => 'email_confirm',
+                                :only_path => false,
+                                :registration_referrer => registration_referrer,
+                                :protocol => 'https')
+      Rails.logger.debug confirmationUrl
+      @user.register(confirmationUrl)
+    rescue Exception => e
+      json = JSON.generate({:errors => {:base => [e.message]}})
+      render :json => json, :status => :internal_server_error and return
+    end
+    
+    if @user.errors.length == 0
+      json = JSON.generate({:result => "Check your inbox for an email with a validation link. Click on the link to complete the registration process."})
+      render :json => json and return
+    else
+      json = JSON.generate({:errors => @user.errors})
+      render :json => json, :status => :internal_server_error and return
+    end
   end
 end
