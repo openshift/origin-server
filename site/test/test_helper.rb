9797eb83baca16c5308153747c74b2ea4ecedaee
Dan McPherson <dmcphers@redhat.com>
2012-01-03 17:47:34 -0500

    moving streamline into site

--- site/test/test_helper.rb
+++ site/test/test_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha'
 require 'openshift'
+require 'streamline'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
