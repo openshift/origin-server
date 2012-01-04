8818caca751f0bd6b52a005258f0b109ac6df8dc
Dan McPherson <dmcphers@redhat.com>
2012-01-03 21:01:43 -0500

    remove unused method

--- site/test/test_helper.rb
+++ site/test/test_helper.rb
@@ -12,33 +12,6 @@ class ActiveSupport::TestCase
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   #fixtures :all
-
-  #
-  # Obtain a unique username from S3.
-  #
-  #   reserved_usernames = A list of reserved names that may
-  #     not be in the global store
-  #
-  def get_unique_username(reserved_usernames=[])
-    result={}
-
-    loop do
-      # Generate a random username
-      chars = ("1".."9").to_a
-      namespace = "unit" + Array.new(8, '').collect{chars[rand(chars.size)]}.join
-      login = "libra-test+#{namespace}@redhat.com"
-      has_txt = OpenShift::Server.has_dns_txt?(namespace)
-      user = OpenShift::User.find(login)
-
-      unless user or has_txt or reserved_usernames.index(login)
-        result[:login] = login
-        result[:namespace] = namespace
-        break
-      end
-    end
-
-    return result
-  end
   
   def setup_session(role='')
     session[:login] = 'tester'
