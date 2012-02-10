a98a76afced8f06c5f700aa610efd57214023136
Clayton Coleman <ccoleman@redhat.com>
2012-02-09 20:33:04 -0500

    Integrate app creation workflow into new console

--- site/app/controllers/application_types_controller.rb
+++ site/app/controllers/application_types_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationTypesController < ApplicationController
+  layout 'console'
+  before_filter :new_forms
+
   def show
     @application_type = ApplicationType.find params[:id]
     @application = RestApi::Application.new
