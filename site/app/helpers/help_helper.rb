dc27d178d9806a9b93bc4f15b7db087bc36081ed
Clayton Coleman <ccoleman@redhat.com>
2012-02-20 13:01:38 -0500

    Pull out help links into their own helper URLs to minimize changes

--- site/app/helpers/help_helper.rb
+++ site/app/helpers/help_helper.rb
@@ -6,6 +6,22 @@ module HelpHelper
     "http://docs.redhat.com/docs/#{locale}/OpenShift_Express/2.0/html/User_Guide/#{topic}"
   end
 
+  def ssh_key_user_guide_topic_url
+    user_guide_topic_url 'sect-User_Guide-Managing_SSH_Keys.html'
+  end
+  
+  def manage_app_cli_user_guide_topic_url
+    user_guide_topic_url 'chap-User_Guide-Application_Development.html'
+  end
+
+  def deploy_hook_user_guide_topic_url
+    user_guide_topic_url 'sect-User_Guide-Using_the_Jenkins_Embedded_Build_System-The_BuildDeploy_Process_in_OpenShift_Express.html'
+  end
+
+  def add_domains_user_guide_topic_url
+    user_guide_topic_url 'sect-User_Guide-Creating_Applications-Creating_Applications_with_the_Command_Line_Interface.html'
+  end
+
   def git_homepage_url
     "http://git-scm.com/"
   end
