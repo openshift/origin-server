b27b6cba342a09fba595097bec840edffac1eb6d
Clayton Coleman <ccoleman@redhat.com>
2012-02-21 11:29:35 -0500

    Add domains controller

---
+++ site/app/controllers/domains_controller.rb
@@ -0,0 +1,29 @@
+class DomainsController < ConsoleController
+  def new
+    @domain = Domain.new
+  end
+
+  def create
+    @domain = Domain.new params[:domain]
+    @domain.as = session_user
+
+    if @domain.save
+      redirect_to account_path, :flash => {:success => 'Your domain has been created'}
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @domain = Domain.first(:as => session_user)
+  end
+
+  def update
+    @domain = Domain.first(:as => session_user).load(params[:domain])
+    if @domain.save
+      redirect_to account_path, :flash => {:success => 'Your domain has been changed.  Your public URLs will now be different'}
+    else
+      render :edit
+    end
+  end
+end
