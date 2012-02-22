0e7a4daf9d08877e3fd47cdeb78beec270b74134
John (J5) Palmieri <johnp@redhat.com>
2012-02-21 19:54:03 -0500

    add tests for the applications list page

--- site/test/functional/applications_controller_test.rb
+++ site/test/functional/applications_controller_test.rb
@@ -6,12 +6,85 @@ class ApplicationsControllerTest < ActionController::TestCase
 #    assert_response :success
 #  end
 
-  test "should retrieve application list" do
-    return unless Rails.configuration.integrated
+  def setup
+    setup_integrated
+  end
+
+  def setup_integrated
+    host = ENV['LIBRA_HOST'] || 'localhost'
+    RestApi::Base.site = "https://#{host}/broker/rest"
+    RestApi::Base.prefix='/broker/rest/'
+
+    @ts = "#{Time.now.to_i}#{gen_small_uuid[0,6]}"
+
+    @user = WebUser.new :email_address=>"app_test1@test1.com", :rhlogin=>"app_test1@test1.com"
+    auth_headers = {'Authorization' => "Basic #{Base64.encode64("#{@user.login}:#{@user.password}").strip}"}
+
+    domain = Domain.new :namespace => "#{@ts}", :as => @user
+    unless domain.save
+      puts domain.errors.inspect
+      fail 'Unable to create the initial domain, test cannot be run'
+    end
     setup_session
-    get(:show)
-    assert assigns(:applications)
+  end
+
+  def setup_session
+    session[:login] = @user.login
+    session[:user] = @user
+    session[:ticket] = '123'
+    @request.cookies['rh_sso'] = '123'
+    @request.env['HTTPS'] = 'on'
+  end
+
+  test "should create and delete app" do
+    post(:create, {:application => get_post_form})
+    app = assigns(:application)
+    assert app
+    assert app.errors.empty?
+
+    delete :destroy, :id => app.id
+    assert_redirected_to applications_path
+  end
+
+  test "should retrieve application list" do
+    post(:create, {:application => get_post_form})
+    app = assigns(:application)
+    assert app
+    assert app.errors.empty?
+
+    form = get_post_form
+    form[:name] = "test2"
+    post(:create, {:application => form})
+    app = assigns(:application)
+    assert app
+    assert app.errors.empty?
+
+    # get a filtered version of the list
+    post(:index, :applications_filter => get_filter_form)
+    apps = assigns(:applications)
+    assert apps
+    assert_equal apps.length, 1
+    assert_equal apps[0].name, 'test2'
     assert_response :success
+
+    # get full version of the list
+    get(:index)
+    apps = assigns(:applications)
+    assert apps
+    assert_equal apps.length, 2
+    apps.each do |app|
+      assert app.name == 'test1' || app.name == 'test2'
+    end
+    assert_response :success
+
+    # delete app
+    assert_difference('assigns(:applications).count', -1) do
+      delete :destroy, :id => 'test2'
+      # for some reason the redirect doesn't update applications
+      # so we call index here to make sure the app is deleted
+      get :index
+    end
+
   end
 
 #  test "should check for empty name" do
@@ -32,6 +105,15 @@ class ApplicationsControllerTest < ActionController::TestCase
 #  end
 
   def get_post_form
-    {:name => 'test1', :application_type => 'empty'}
+    {:name => 'test1', :application_type => 'raw-0.1'}
+  end
+
+  def get_filter_form
+    {:name => 'test2'}
+  end
+
+  def teardown
+    domain = Domain.first :as => @user
+    domain.destroy_recursive if domain
   end
 end
