f995478040b6dc8511ddf3ff7216b2ebf0391c4b
Fabiano Franz <ffranz@redhat.com>
2012-03-01 16:56:48 -0300

    Fixes 798502: added links to docs

--- site/app/helpers/help_helper.rb
+++ site/app/helpers/help_helper.rb
@@ -6,6 +6,10 @@ module HelpHelper
     "http://docs.redhat.com/docs/#{locale}/OpenShift_Express/2.0/html/User_Guide/#{topic}"
   end
 
+  def community_topic_url(topic)
+    "https://www.redhat.com/openshift/community/#{topic}"
+  end
+
   def ssh_key_user_guide_topic_url
     user_guide_topic_url 'sect-User_Guide-Managing_SSH_Keys.html'
   end
@@ -21,9 +25,33 @@ module HelpHelper
   def add_domains_user_guide_topic_url
     user_guide_topic_url 'sect-User_Guide-Creating_Applications-Creating_Applications_with_the_Command_Line_Interface.html'
   end
+  
+  def manage_cartridges_user_guide_topic_url
+    user_guide_topic_url 'sect-User_Guide-Adding_and_Managing_Database_Instances.html#form-User_Guide-Adding_Database_Back_Ends_to_Your_Applications-Command_Options_for_Controlling_Cartridges'
+  end
+  
+  def git_user_guide_topic_url
+    user_guide_topic_url 'sect-User_Guide-Application_Development-Deploying_Applications.html'
+  end
+  
+  def install_cli_knowledge_base_url
+    community_topic_url 'kb/kb-e1000/installing-openshift-express-client-tools-on-non-rpm-based-systems'
+  end
 
   def post_to_forum_url
-    'https://www.redhat.com/openshift/community/forums/express'
+    community_topic_url 'forums/express'
+  end
+  
+  def sync_git_with_remote_repo_knowledge_base_url
+    community_topic_url 'kb/kb-e1006-sync-new-express-git-repo-with-your-own-existing-git-repo'
+  end
+  
+  def rails_quickstart_guide
+    community_topic_url 'kb/kb-e1005-ruby-on-rails-express-quickstart-guide'
+  end
+  
+  def user_guide
+    user_guide_topic_url 'index.html'
   end
 
   def git_homepage_url
--- site/app/models/embedded.rb
+++ site/app/models/embedded.rb
@@ -1,19 +1,12 @@
 #
 # The REST API model object representing an embedded cartridge name attached to an application.
+# This is going to be DEPRECATED on server side, so we will not use it (use /cartridges instead)
 #
 class Embedded < RestApi::Base
   
   self.collection_name = 'embedded'
   
-  schema do
-    string :name
-  end
-
   def initialize(attributes={}, persisted=false)
-    @persisted = persisted
-    @as = attributes[:as]
-    attributes.delete :as
-    # super attributes
   end
   
   belongs_to :application
