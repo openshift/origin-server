6606d443fa86a226d8227769d3b34cb0b2268da7
Clayton Coleman <ccoleman@redhat.com>
2012-03-05 12:37:46 -0500

    Reenable node.js

--- site/app/models/application_type.rb
+++ site/app/models/application_type.rb
@@ -90,7 +90,7 @@ class ApplicationType
       :id => 'nodejs-0.6',
       :name => 'Node.js 0.6',
       :version => 'Node.js 0.6.10',
-      :categories => [:new], # removed :framework tag
+      :categories => [:framework, :new], # removed :framework tag
       :description => 'Node.js is a platform built on Chrome\'s JavaScript runtime for easily building fast, scalable network applications. Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient, perfect for data-intensive real-time applications that run across distributed devices.',
       :website => 'http://rack.rubyforge.org/'
     },
