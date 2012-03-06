32b2e623990618d97dc17df610154a5100759886
John (J5) Palmieri <johnp@redhat.com>
2012-03-05 19:11:21 -0500

    get cart list from rest api but fill in details in the model
    * right now the rest api only gives us a list of the names of the
      cartridges.
    * fill in the details until backend supports API for getting
      metadata for each cart

--- site/app/controllers/cartridge_types_controller.rb
+++ site/app/controllers/cartridge_types_controller.rb
@@ -1,8 +1,9 @@
 class CartridgeTypesController < ConsoleController
+  
 
   def index
     app_id = params[:application_id]
-    types = CartridgeType.find :all
+    types = CartridgeType.find :all, {:as=> session_user}
     @cart_types = types
     # TODO: add categories
     #@cart_types, types = types.partition { |t| t.categories.include?(:storage) }
--- site/app/models/cartridge_type.rb
+++ site/app/models/cartridge_type.rb
@@ -1,28 +1,39 @@
-class CartridgeType
+class CartridgeType < RestApi::Base
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
   class NotFound < StandardError
   end
 
-  attr_accessor :id, :name, :version, :description
+  schema do
+    string :name, :type
+  end
+
+  attr_accessor :name, :id, :version, :description
   attr_accessor :provides
   attr_accessor :cartridge
   attr_accessor :website, :license, :license_url
   attr_accessor :categories, :learn_more_url
   attr_accessor :help_topics
 
+  self.prefix = "#{RestApi::Base.site.path}/cartridges/embedded"
+
   def initialize(attributes={})
     attributes.each do |name,value|
       send("#{name}=", value)
     end
+    super
   end
 
   def persisted?
     true
   end
 
-  @default_types = [
+  # FIXME: Right now the restapi only gives the name (id) of the available
+  #        types so we supliment it with info here.  Ideally the REST API
+  #        or some common lookaside cache populates this data
+  @type_map = {
+    "mongodb-2.0" =>
     {
       :id => 'mongodb-2.0',
       :name => 'MongoDB NoSQL Database 2.0',
@@ -35,28 +46,41 @@ class CartridgeType
       :help_topics => {
       }
     },
+    "mysql-5.1" =>
     {
       :id => 'mysql-5.1',
       :name => 'MySQL Database 5.1',
       :version => 'MySQL 5.1',
       :license => 'GPLv2 with exceptions',
       :license_url => 'http://www.mysql.com/about/legal/licensing/index.html',
-      :categories => [:cartridge],
+      :categories => [:embedded],
       :description => 'MySQL is a multi-user, multi-threaded SQL database server.',
       :website => 'http://www.mysql.com/',
       :help_topics => {
       }
     }
-  ].map { |t| CartridgeType.new t }
+  }
 
   class << self
     def find(*arguments)
-      option = arguments.slice(0)
-      case option
+      scope   = arguments.slice!(0)
+      options = arguments.slice!(0) || {}
+      case scope
       when String
         @default_types.find { |type| type.id == option } or raise NotFound
       when :all
-        Array.new(@default_types)
+        path = "#{prefix}.json"
+        rest_types = format.decode(connection(options).get(path, headers).body)
+
+        default_types = rest_types.map do |t|
+          name = t['name']
+
+          if !@type_map[name].nil?
+            CartridgeType.new(@type_map[name])
+          else
+            CartridgeType.new({:id => name, :name => name, :categories => [t['type']]})
+          end
+        end
       when Symbol
         @default_types.find { |type| type.categories.include? option }
       else
