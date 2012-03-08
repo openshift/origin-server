d083230c63b885970b2166a44521be1a7c32c238
John (J5) Palmieri <johnp@redhat.com>
2012-03-08 18:57:05 -0500

    when listing available carts show installed carts but disable selection

--- site/app/controllers/cartridge_types_controller.rb
+++ site/app/controllers/cartridge_types_controller.rb
@@ -1,12 +1,25 @@
 class CartridgeTypesController < ConsoleController
-  
 
   def index
     @application_id = params[:application_id]
+
+    @domain = Domain.first :as => session_user
+    @application = @domain.find_application @application_id
+ 
     types = CartridgeType.find :all, {:as=> session_user}
+    installed_carts = @application.cartridges
+
+    @installed_cart_types = []
+    installed_carts.each do |cart|
+      installed_cart_type = types.find { |t| t.id == cart.name }
+      if !installed_cart_type.nil?
+        installed_cart_type.categories.push :installed
+      end
+    end
+
+    # TODO: further categorization
+    @installed_cart_types, types = types.partition { |t| t.categories.include?(:installed) }
     @cart_types = types
-    # TODO: add categories
-    #@cart_types, types = types.partition { |t| t.categories.include?(:storage) }
   end
 
   def show
