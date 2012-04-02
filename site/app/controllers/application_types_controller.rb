39fce160be2fca1421f0b8d62c9ca18ca59122ee
John (J5) Palmieri <johnp@redhat.com>
2012-04-02 12:47:44 -0400

    [ui] only show gear size if there is more than one option
     * restrict option to "small" gear size.  Once we can get the options
       from the server the gear sizes should show up in the UI

--- site/app/controllers/application_types_controller.rb
+++ site/app/controllers/application_types_controller.rb
@@ -12,6 +12,7 @@ class ApplicationTypesController < ConsoleController
     @application = Application.new :as => session_user
 
     # hard code for now but we want to get this from the server eventually
-    @gear_sizes = ["small", "medium"]
+    @gear_sizes = ["small"] # gear size choice only shows if there is more than
+                            # one option
   end
 end
