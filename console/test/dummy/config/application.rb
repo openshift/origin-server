6ac9f07438a44785224c69f25be239401a079425
Clayton Coleman <ccoleman@redhat.com>
2012-04-08 19:20:42 -0400

    Don't require rails/all, no need for activerecord

--- console/test/dummy/config/application.rb
+++ console/test/dummy/config/application.rb
@@ -1,6 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "active_resource/railtie"
+require "rails/test_unit/railtie"
 
 Bundler.require
 require "console/engine"
