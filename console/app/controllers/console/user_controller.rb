64958a4774c204d79188bf7416a63e46c4a48993
Clayton Coleman <ccoleman@redhat.com>
2012-04-08 17:18:09 -0400

    Begin rearranging user_controller to be easily overriden

---
+++ console/app/controllers/console/user_controller.rb
@@ -0,0 +1,12 @@
+class Console::UserController < SiteController
+  layout 'console'
+
+  before_filter :require_login, :only => :show
+
+  def show
+    @user = session_user
+    @domain = Domain.find :first, :as => session_user
+    @keys = Key.find(:all, :as => session_user)
+    render :layout => 'console'
+  end
+end
