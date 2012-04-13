dad7ba8291203debe57eac5d89f1b81c1638e9c3
Clayton Coleman <ccoleman@redhat.com>
2012-04-13 00:23:36 -0400

    Application controller is in dummy app

--- console/test/dummy/app/controllers/application_controller.rb
+++ console/test/dummy/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def session_user
+    # return the current authenticated user or nil
+  end
+  def require_login
+    # this method should test authentication, or redirect the user
+    # to the correct location
+  end
+  def logged_in?
+    not session_user.nil?
+  end
 end
