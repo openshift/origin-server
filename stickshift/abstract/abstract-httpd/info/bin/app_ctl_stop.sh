7b9a30f7980667a2153d355ad72a86c416ff6018
Rob Millner <rmillner@redhat.com>
2012-05-02 11:57:24 -0700

    Add pre/post start/stop hooks to abstract_httpd.

--- stickshift/abstract/abstract-httpd/info/bin/app_ctl_stop.sh
+++ stickshift/abstract/abstract-httpd/info/bin/app_ctl_stop.sh
@@ -12,7 +12,9 @@ done
 CART_CONF_DIR=${CARTRIDGE_BASE_PATH}/${OPENSHIFT_GEAR_TYPE}/info/configuration/etc/conf
 
 # Stop the app
+run_user_hook pre stop
 set_app_state stopped
 httpd_pid=`cat ${OPENSHIFT_RUN_DIR}httpd.pid 2> /dev/null`
 /usr/sbin/httpd -C "Include ${OPENSHIFT_GEAR_DIR}conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
 wait_for_stop $httpd_pid
+run_user_hook post stop
