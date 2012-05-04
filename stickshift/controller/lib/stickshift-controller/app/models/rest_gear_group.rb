d0d22ffa069b3bc260b22556ab971249675817fd
Krishna Raman <kraman@gmail.com>
2012-05-04 11:35:55 -0700

    Changing cartridge type attribute to name to remain consistent with rest of API

--- stickshift/controller/lib/stickshift-controller/app/models/rest_gear_group.rb
+++ stickshift/controller/lib/stickshift-controller/app/models/rest_gear_group.rb
@@ -5,8 +5,8 @@ class RestGearGroup < StickShift::Model
     app             = group_instance.app
     self.name       = group_instance.name
     self.gears      = group_instance.gears.map{ |gear| {:id => gear.uuid} }
-    self.cartridge = group_instance.component_instances.map{ |comp_inst| {:type => app.comp_instance_map[comp_inst].parent_cart_name}}
-    self.cartridge.delete_if{ |comp| comp[:type] == app.name }
+    self.cartridge = group_instance.component_instances.map{ |comp_inst| {:name => app.comp_instance_map[comp_inst].parent_cart_name}}
+    self.cartridge.delete_if{ |comp| comp[:name] == app.name }
   end
 
   def to_xml(options={})
