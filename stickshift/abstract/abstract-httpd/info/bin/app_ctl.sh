24fdff73394eca3c62a3da2bcd3ad5fe22e0a499
Ram Ranganathan <ramr@redhat.com>
2012-05-10 16:54:38 -0700

    Revert to cartridge type -- no app types any more.

--- stickshift/abstract/abstract-httpd/info/bin/app_ctl.sh
+++ stickshift/abstract/abstract-httpd/info/bin/app_ctl.sh
@@ -21,18 +21,20 @@ validate_run_as_user
 
 . app_ctl_pre.sh
 
-CART_CONF_DIR=${CARTRIDGE_BASE_PATH}/${OPENSHIFT_GEAR_TYPE}/info/configuration/etc/conf
+CART_CONF_DIR=${CARTRIDGE_BASE_PATH}/${CARTRIDGE_TYPE}/info/configuration/etc/conf
+
+cart_instance_dir=${OPENSHIFT_HOMEDIR}/${CARTRIDGE_TYPE}
 
 case "$1" in
     start)
         _state=`get_app_state`
-        if [ -f ${OPENSHIFT_GEAR_DIR}run/stop_lock -o idle = "$_state" ]; then
+        if [ -f $cart_instance_dir/run/stop_lock -o idle = "$_state" ]; then
             echo "Application is explicitly stopped!  Use 'rhc app start -a ${OPENSHIFT_GEAR_NAME}' to start back up." 1>&2
             exit 0
         else
             src_user_hook pre_start_${CARTRIDGE_TYPE}
             set_app_state started
-            /usr/sbin/httpd -C "Include ${OPENSHIFT_GEAR_DIR}conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
+            /usr/sbin/httpd -C "Include $cart_instance_dir/conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
             run_user_hook post_start_${CARTRIDGE_TYPE}
         fi
     ;;
@@ -42,7 +44,7 @@ case "$1" in
     restart|graceful)
         src_user_hook pre_start_${CARTRIDGE_TYPE}
         set_app_state started
-        /usr/sbin/httpd -C "Include ${OPENSHIFT_GEAR_DIR}conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
+        /usr/sbin/httpd -C "Include $cart_instance_dir/conf.d/*.conf" -f $CART_CONF_DIR/httpd_nolog.conf -k $1
         run_user_hook post_start_${CARTRIDGE_TYPE}
     ;;
 esac
