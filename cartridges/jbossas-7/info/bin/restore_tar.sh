81de224d705ae89edc2f0cb40cf66a2386836d75
Rob Millner <rmillner@redhat.com>
2012-05-18 11:17:43 -0700

    Data directory moved to ~/app

--- cartridges/jbossas-7/info/bin/restore_tar.sh
+++ cartridges/jbossas-7/info/bin/restore_tar.sh
@@ -10,9 +10,9 @@ done
 
 if [ "$include_git" = "INCLUDE_GIT" ]
 then
-  echo "Restoring ~/git/${OPENSHIFT_GEAR_NAME}.git, ~/${OPENSHIFT_GEAR_NAME}/data and ~/.m2" 1>&2
-  /bin/tar --strip=2 --overwrite -xmz "./*/${OPENSHIFT_GEAR_NAME}/data" "./*/git" "./*/.m2" --exclude="./*/git/${OPENSHIFT_GEAR_NAME}.git/hooks" 1>&2  
+  echo "Restoring ~/git/${OPENSHIFT_GEAR_NAME}.git, ~/app/data and ~/.m2" 1>&2
+  /bin/tar --strip=2 --overwrite -xmz "./*/app/data" "./*/git" "./*/.m2" --exclude="./*/git/${OPENSHIFT_GEAR_NAME}.git/hooks" 1>&2  
 else
-  echo "Restoring ~/${OPENSHIFT_GEAR_NAME}/data" 1>&2
-  /bin/tar --strip=2 --overwrite -xmz "./*/${OPENSHIFT_GEAR_NAME}/data" 1>&2
+  echo "Restoring ~/app/data" 1>&2
+  /bin/tar --strip=2 --overwrite -xmz "./*/app/data" 1>&2
 fi
