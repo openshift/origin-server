adb75338e7b54fad275d46118b45e4d2843f92fa
Fotios Lindiakos <fotios@redhat.com>
2012-06-18 18:04:49 -0400

    BZ828116: Added logic for displaying credentials for applications created from templates
    Descriptor changes:
     - Automatically sorting descriptors to see changes in diffs better
     - Regenerated descriptors without doc-root
     - Added credentials metadata to drupal and wordpress templates

--- site/app/helpers/layout_helper.rb
+++ site/app/helpers/layout_helper.rb
@@ -31,8 +31,9 @@ module LayoutHelper
     return if @flashed_once || flash.nil?; @flashed_once = true
     tags = []
     flash.each do |key, value|
-      next if value.blank?
-      tags << content_tag(flash_element_for(key), value, :class => alert_class_for(key))
+      (value.kind_of?(Array) ? value : [value]).each do |value|
+        tags << content_tag(flash_element_for(key), value, :class => alert_class_for(key))
+      end
     end
     content_tag(:div, tags.join.html_safe, :id => 'flash') unless tags.empty?
   end
