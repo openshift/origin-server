9b189787ee23fba9a36a351189741e8e8af2436c
Dan Mace <ironcladlou@gmail.com>
2012-07-06 10:39:30 -0400

    Refactor JBoss hot deployment support
    Link the JBoss deployments directory to the app repo deployments
    directory to reduce file duplication. Extract the configure hook
    and Jenkins build template into the abstract JBoss cart to reduce
    code duplication. Provide hot deployment support for both the AS7
    and EAP 6 carts uniformly via abstract cart refactoring.

--- stickshift/abstract/abstract-jboss/info/bin/deploy.sh
+++ stickshift/abstract/abstract-jboss/info/bin/deploy.sh
@@ -28,13 +28,4 @@ else
   fi
 fi
 
-# Sync everything in the repo deployments directory with the live
-# JBoss deployments directory.
-sync_source="${OPENSHIFT_REPO_DIR}/deployments/"
-sync_desc="${OPENSHIFT_GEAR_DIR}${OPENSHIFT_GEAR_TYPE}/standalone/deployments/"
-
-echo "Syncing Git deployments directory [${sync_source}] with JBoss deployments directory [${sync_desc}]"
-
-rsync -vr --delete --exclude=*.git --exclude=.gitignore $sync_source $sync_desc
-
 user_deploy.sh
