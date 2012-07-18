0ae9b9c3665f2a451731c8f3c4b425dd3052b3bd
Clayton Coleman <ccoleman@redhat.com>
2012-07-17 22:13:04 -0400

    Add the necessary glue so that the checkbox for tax exemption shows up and sets values on creation.  Also ensure that user accounts are created with the default tax_exempt value of 0.

--- site/app/models/user.rb
+++ site/app/models/user.rb
@@ -4,11 +4,21 @@
 class User < RestApi::Base
   singleton
 
-  has_many :keys
-
   schema do
-    string :login
+    string :login, :plan_id
   end
 
+  has_many :keys
   has_many :domains
+
+  def plan_id
+    super.to_sym
+  end
+
+  def plan
+    @plan ||= Plan.find plan_id
+  end
+  def plan=(plan)
+    @plan_id = plan.is_a?(String) ? plan : plan.id
+  end
 end
