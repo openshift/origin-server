907b2f208584916a9663d607bccc7655650f3aac
Adam Miller <admiller@redhat.com>
2012-07-23 16:59:05 -0500

    BROKE THE BUILD
    Revert "BZ 841681: Make update_yaml single instance and use tmp file for generating/updating facts."
    
    This reverts commit a8477c9ef54010adfca698593f9543181b605c47.

--- gearchanger/mcollective/agent/facts/update_yaml.rb
+++ gearchanger/mcollective/agent/facts/update_yaml.rb
@@ -3,14 +3,4 @@
 require 'facter'
 require 'yaml'
 
-def single_instance(&block)
-  if File.open($0).flock(File::LOCK_EX|File::LOCK_NB)
-    block.call
-  else
-    warn "Script #{ $0 } is already running"
-  end
-end
-
-single_instance do
-  puts YAML.dump(Facter.to_hash)
-end
+puts YAML.dump(Facter.to_hash)
