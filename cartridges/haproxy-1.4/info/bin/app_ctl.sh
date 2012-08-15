0128dee38a40c6b829ad8b6f8798c5cb795bff77
Ram Ranganathan <ramr@redhat.com>
2012-08-15 10:45:25 -0700

    Add check for cartridge type before federating the call.

--- cartridges/haproxy-1.4/info/bin/app_ctl.sh
+++ cartridges/haproxy-1.4/info/bin/app_ctl.sh
@@ -206,7 +206,7 @@ function status() {
 # Operate on the app server control script first.
 CART_INFO_DIR=$(dirname "$0")
 APP_CTL_PATH=$(echo "$PATH" | sed "s#${CART_INFO_DIR}/:##")
-PATH=$APP_CTL_PATH app_ctl.sh "$@"
+[[ "$CARTRIDGE_TYPE" =~ haproxy* ]]  ||  PATH=$APP_CTL_PATH app_ctl.sh "$@"
 
 # And then on the haproxy and haproxy_ctld.
 case "$1" in
