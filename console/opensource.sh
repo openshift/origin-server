3b0fbee5f3d1d782c814ad457bd74a7d20c2f816
Clayton Coleman <ccoleman@redhat.com>
2012-08-28 23:27:44 -0400

    Test and verify the steps for history rewriting

--- console/opensource.sh
+++ console/opensource.sh
@@ -1,17 +1,11 @@
-if [ -d "../console" ]; then
-  echo The directory ../console already exists.  To run you must 'rm -rf ../console'
-  exit 1
-fi
-if [ -d "../console_clean" ]; then
-  echo The directory ../console_clean already exists.  To run you must 'rm -rf ../console_clean'
-  exit 1
-fi
-
-# run from the the li/ dir
-pushd ..
-
 # Use a simpler time format for values
 export TIMEFORMAT=$'  Completed in %Rs\n'
+export DIR=foo
+export REPO=test_git2
+export REPO_WORKING=${REPO}_working
+export REPO_CLEAN=${REPO}_clean
+
+exit
 
 # Create a utility script to remove empty branch parents
 cat > /tmp/filter_empty_branch.rb <<FILTER_EMPTY_BRANCH_RB
@@ -29,28 +23,28 @@ FILTER_EMPTY_BRANCH_RB
 chmod 770 /tmp/filter_empty_branch.rb
 
 # Initial working repository setup
-git clone --no-hardlinks li console
-pushd console/
+git clone --no-hardlinks $REPO $REPO_WORKING
+pushd $REPO_WORKING/
 git checkout master
 
 # Preserve selenium
-git mv selenium/Gemfile site/test/Gemfile_old
-git mv selenium/Gemfile.lock site/test/Gemfile_old.lock
-git mv selenium/ site/test/
-git commit -m "Move selenium into the site"
+#git mv selenium/Gemfile site/test/Gemfile_old
+#git mv selenium/Gemfile.lock site/test/Gemfile_old.lock
+#git mv selenium/ site/test/
+#git commit -m "Move selenium into the site"
 
 # Make any other repository changes here
 
 # Generate the list of paths that we want to save from the version history
 # to_keep.txt generated with find . -type f and then excluding things that we don't want
-pushd site
+pushd $DIR
 cp .opensource_include /tmp/valid_sources.txt
 find . -type f | grep -vf .opensource_exclude | sort -u >> /tmp/valid_sources.txt
 
 echo Generating script to separate content that went into the new repository
 find . | grep -f /tmp/valid_sources.txt | sort -u | xargs -n 1 echo git rm > /tmp/delete_moved.sh
 chmod 770 /tmp/delete_moved.sh
-echo '  Run /tmp/delete_moved.sh from the li/site dir to remove all content that was copied into the new repository.'
+echo '  Run /tmp/delete_moved.sh from the li/$DIR dir to remove all content that was copied into the new repository.'
 
 echo Calculating all historical paths for valid files
 time cat /tmp/valid_sources.txt | xargs -P 4 -n 1 git log --pretty=oneline --follow --name-only | grep -v ' ' | sort -u > /tmp/paths_to_keep.txt
@@ -73,27 +67,27 @@ git branch -a | egrep -v "master|origin/master|origin/HEAD" |
     git branch -r -d "${branch/remotes\//}"
   done
 
-# remove unnecessary tags
-time git tag -l | grep -vE "^0\.|rhc-0|rhc-site-0" | while read tag; do git tag -d "${tag}"; done
-
+# remove all tags
+time git tag -l | while read tag; do git tag -d "${tag}"; done
 
 echo Rewriting git history to contain only paths listed in /tmp/paths_to_keep.txt
 
 # Remove all copies of non desired files from the history
-time git filter-branch $GIT_TEMP --prune-empty -f --tag-name-filter cat --index-filter 'git ls-tree -r --name-only --full-tree $GIT_COMMIT | grep -vf /tmp/paths_to_keep.txt | xargs git rm -qrf --cached --ignore-unmatch' -- --all
+time git filter-branch $GIT_TEMP --prune-empty -f --tag-name-filter cat --index-filter 'git ls-tree -r --name-only --full-tree $GIT_COMMIT | grep -vf /tmp/paths_to_keep.txt | xargs -r git rm -qrf --cached --ignore-unmatch' -- --all
 
 echo Removing empty branches and merge commits
 time git filter-branch $GIT_TEMP --prune-empty -f --tag-filter-name cat --parent-filter /tmp/filter_empty_branch.rb -- --all
 
-echo Rewriting git history to move all /site content to /
+echo Rewriting git history to move all /$DIR content to /
+time git filter-branch $GIT_TEMP --prune-empty -f --subdirectory-filter $DIR -- --all
 
 # Reroot the site dir at root
-echo 'To reroot /site to / (as last final step)'
-echo 'time git filter-branch $GIT_TEMP --prune-empty -f --tag-name-filter cat --subdirectory-filter site -- --all'
+echo 'To reroot /$DIR to / (as last final step)'
+echo 'time git filter-branch $GIT_TEMP --prune-empty -f --tag-name-filter cat --subdirectory-filter $DIR -- --all'
 
 popd
-git clone --no-hardlinks console console_clean
-pushd console_clean
+git clone --no-hardlinks $REPO_WORKING $REPO_CLEAN
+pushd $REPO_CLEAN/
 
 # Garbage collect old revisions and versions now that we've deleted content
 git reset --hard
@@ -104,52 +98,9 @@ git gc --aggressive --prune=now
 echo Total size of repository: `du -sh`
 echo Git rewrite complete, repository contains only the desired history
 echo ----------------------------------
-echo 'TODO: Ensure all versions are explicit in Gemfile'
-echo 'TODO: Merge the selenium gemfiles into root'
-echo 'TODO: Reintroduce web_user and add base helpers'
 
-# Add base engine files
-cat > console.spec <<CONSOLE_SPEC
-# encoding: utf-8
-require File.expand_path('../lib/console/version', __FILE__)
-
-Gem::Specification.new do |gem|
-  gem.authors = ["Clayton Coleman"]
-  gem.description = %q{The OpenShift application console is a Rails engine that provides an easy-to-use interface for managing your OpenShift applications.}
-  gem.email = ['smarterclayton@gmail.com']
-  gem.files = Dir['Gemfile', 'LICENSE.md', 'README.md', 'Rakefile', 'app/**/*', 'config/**/*', 'lib/**/*', 'public/**/*']
-  gem.homepage = 'https://github.com/openshift/console'
-  gem.name = 'console'
-  gem.require_paths = ['lib']
-  #gem.required_rubygems_version = Gem::Requirement.new('>= 1.3.6')
-  gem.summary = %q{Openshift Application Console}
-  gem.test_files = Dir['test/**/*']
-  gem.version = Console::VERSION
-end
-CONSOLE_SPEC
-
-cat > lib/console.rb <<CONSOLE_RB
-require 'console/engine.rb'
-require 'console/version.rb'
-module Console
-end
-CONSOLE_RB
-
-mkdir lib/console/
-
-cat > lib/console/engine.rb <<ENGINE_RB
-require 'rails/engine'
-
-module Console
-  class Engine < Rails::Engine
-end
-ENGINE_RB
-
-cat > lib/console/version.rb <<VERSION_RB
-module Console
-  VERSION = 1.0.0
-end
-VERSION_RB
-
-git add .
-git commit -m 'Create stub engine files'
+echo To merge this
+echo $ git remote add -f CONSOLE /path/to/li
+echo $ git merge -s ours --no-commit CONSOLE/master
+echo $ git read-tree --prefix=console/ -u CONSOLE/master
+echo $ git commit
