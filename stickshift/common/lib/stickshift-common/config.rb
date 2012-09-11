7b35c216d447751f9a96015a4360d65d143776cc
Miciah Masters <miciah.masters@gmail.com>
2012-09-11 21:18:19 +0000

    Centralize plug-in configuration
    Change gearchanger-mcollective-plugin, swingshift-mongo-plugin, and
    uplift-bind-plugin to get configuration from /etc/stickshift/plugins.d/*.
    
    The motivation is that an administrator should not need to edit Ruby code
    in order to configure the broker.  See US2783 on Rally.

--- stickshift/common/lib/stickshift-common/config.rb
+++ stickshift/common/lib/stickshift-common/config.rb
@@ -1,12 +1,12 @@
 #--
 # Copyright 2010 Red Hat, Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,45 +14,35 @@
 # limitations under the License.
 #++
 
-
 require 'rubygems'
-require 'singleton'
 require 'parseconfig'
 
 module StickShift
-  # == StickShift Config
-  #
-  # Allows access to stickshift-node config file.
-  #
-  # Reads config entried for the sdk from /etc/ss/stickshift-node.conf and if
-  # that is not available then it will read it from conf/stickshift-node.conf 
-  # within the ruby gem.
   class Config
-    include Object::Singleton
-
-    CONF_NAME = 'stickshift-node.conf'
     CONF_DIR = '/etc/stickshift/'
-    
-    def initialize()
-      _linux_cfg = File.join(CONF_DIR,CONF_NAME)
-      _gem_cfg = File.join(File.expand_path(File.dirname(__FILE__) + 
-            '/../../conf'), CONF_NAME)
-      @config_path = File.exists?(_linux_cfg) ? _linux_cfg : _gem_cfg
+    PLUGINS_DIR = File.join(CONF_DIR, 'plugins.d/')
+    NODE_CONF_FILE = File.join(CONF_DIR, 'stickshift-node.conf')
 
+    def initialize(conf_path=NODE_CONF_FILE)
       begin
-        @@global_config = ParseConfig.new(@config_path)
+        @conf = ParseConfig.new(conf_path)
       rescue Errno::EACCES => e
-        puts "Could not open config file: #{e.message}"
+        puts "Could not open config file #{conf_path}: #{e.message}"
         exit 253
       end
     end
 
     def get(name)
-      val = @@global_config.get_value(name)
+      val = @conf.get_value(name)
       val.gsub!(/\\:/,":") if not val.nil?
       val.gsub!(/[ \t]*#[^\n]*/,"") if not val.nil?
       val = val[1..-2] if not val.nil? and val.start_with? "\""
       val
+    end
+
+    def get_bool(name)
+      # !! is used to normalise the value to either a 1 (true) or a 0 (false).
+      !!(get(name) =~ /^(true|t|yes|y|1)$/i)
     end
   end
 end
