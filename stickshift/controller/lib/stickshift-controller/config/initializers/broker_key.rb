5751201978c8487a5f6ebb94dbc87c5e1976f1e7
Brenton Leanhardt <bleanhar@redhat.com>
2012-09-26 16:21:21 -0400

    Keeping the controller engine from overwriting the auth config if it's already set

--- stickshift/controller/lib/stickshift-controller/config/initializers/broker_key.rb
+++ stickshift/controller/lib/stickshift-controller/config/initializers/broker_key.rb
@@ -1,8 +1,12 @@
 Broker::Application.configure do
-  config.auth = {
-    :salt           => "ClWqe5zKtEW4CJEMyjzQ",
-    :privkeyfile    => "/etc/stickshift/server_priv.pem",
-    :privkeypass    => "",
-    :pubkeyfile     => "/etc/stickshift/server_pub.pem",
-  }
+  # If use chooses to inject their auth config before the controller loads we
+  # should _not_ overwrite it
+  unless config.respond_to? :auth
+    config.auth = {
+      :salt           => "ClWqe5zKtEW4CJEMyjzQ",
+      :privkeyfile    => "/etc/stickshift/server_priv.pem",
+      :privkeypass    => "",
+      :pubkeyfile     => "/etc/stickshift/server_pub.pem",
+    }
+  end
 end
