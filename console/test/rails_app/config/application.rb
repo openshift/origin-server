3dd80a80901a1bd3d16b9c6531ff44c00a673b49
Clayton Coleman <ccoleman@redhat.com>
2012-09-27 15:40:31 -0400

    ExecJS no longer needs spidermonkey with httpd_execmem selinux permission set

--- console/test/rails_app/config/application.rb
+++ console/test/rails_app/config/application.rb
@@ -5,10 +5,6 @@ require "action_mailer/railtie"
 require "active_resource/railtie"
 require "rails/test_unit/railtie"
 
-# We prereq the yum package 'js' which contains SpiderMonkey.  Explicitly force
-# support for that here to prevent Node (which supplies V8) from conflicting.
-ENV['EXECJS_RUNTIME'] = 'SpiderMonkey'
-
 Bundler.require(*Rails.groups(:default, :assets => %w(development test))) if defined?(Bundler)
 require "console/engine"
 
