0e22ab152d90c2dc724d0104a6936affbb1c049a
Jhon Honce <jhonce@redhat.com>
2012-10-06 08:16:48 -0700

    Port of Idler

---
+++ node-util/bin/oo-restorer-wrapper.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/oddjob_request -s com.redhat.oddjob_openshift -o /com/redhat/oddjob/openshift -i com.redhat.oddjob_restorer restore $1
---
+++ node-util/var/www/html/restorer.php
@@ -0,0 +1,10 @@
+<?php
+
+list($blank, $uuid, $blank) = split("/", $_SERVER["PATH_INFO"]);
+shell_exec("/usr/bin/oo-restorer-wrapper.sh $uuid");
+
+sleep(2);
+$url=str_replace("/$uuid", "", $_SERVER["PATH_INFO"]);
+header("Location: $url");
+
+?>
