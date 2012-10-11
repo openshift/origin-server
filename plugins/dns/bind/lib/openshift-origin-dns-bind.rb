78ecad3241164927e479c469b2aa92529fec8928
Krishna Raman <kraman@gmail.com>
2012-10-11 05:40:28 -0700

    Making openshift-origin-msg-broker-mcollective a Rails engine so that it can hook into Rails initializers Making openshift-origin-dns-bind a Rails engine so that it can hook into Rails initializers

--- plugins/dns/bind/lib/openshift-origin-dns-bind.rb
+++ plugins/dns/bind/lib/openshift-origin-dns-bind.rb
@@ -1,3 +1,10 @@
 require "openshift-origin-common"
-require "openshift-origin-dns-bind/openshift/bind_plugin.rb"
+
+module OpenShift
+  module BindDnsModule
+    require 'openshift-origin-dns-bind/engine/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  end
+end
+
+require "openshift-origin-dns-bind/lib/openshift/bind_plugin.rb"
 OpenShift::DnsService.provider=OpenShift::BindPlugin
