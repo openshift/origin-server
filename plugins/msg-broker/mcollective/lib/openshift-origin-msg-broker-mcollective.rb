d39de422afd4a5f7dd9b53e12b0c490be9c3b4e8
Krishna Raman <kraman@gmail.com>
2012-10-11 05:40:28 -0700

    Making openshift-origin-msg-broker-mcollective a Rails engine so that it can hook into Rails initializers Making openshift-origin-dns-bind a Rails engine so that it can hook into Rails initializers

--- plugins/msg-broker/mcollective/lib/openshift-origin-msg-broker-mcollective.rb
+++ plugins/msg-broker/mcollective/lib/openshift-origin-msg-broker-mcollective.rb
@@ -1,4 +1,10 @@
 require "openshift-origin-common"
-require "openshift-origin-msg-broker-mcollective/config/initializers/openshift-origin-msg-broker-mcollective.rb"
-require "openshift-origin-msg-broker-mcollective/openshift/mcollective_application_container_proxy.rb"
+
+module OpenShift
+  module McollectiveMsgBrokerModule
+    require 'openshift-origin-msg-broker-mcollective/engine/engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
+  end
+end
+
+require "openshift-origin-msg-broker-mcollective/lib/openshift/mcollective_application_container_proxy.rb"
 OpenShift::ApplicationContainerProxy.provider=OpenShift::MCollectiveApplicationContainerProxy
