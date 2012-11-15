616bcd08494ea49e00da700faab4742927b32a04
Krishna Raman <kraman@gmail.com>
2012-11-15 11:37:42 -0800

    F18 compatibility fixes   - apache 2.4   - mongo journaling   - JDK 7   - parseconfig gem update Bugfix for Bind DNS plugin

--- plugins/dns/bind/lib/bind_dns_engine.rb
+++ plugins/dns/bind/lib/bind_dns_engine.rb
@@ -3,8 +3,5 @@ require 'rails'
 
 module OpenShift
   class BindDnsEngine < Rails::Engine
-    paths.lib                  << "lib/openshift-origin-bind-dns/lib"
-    paths.config               << "lib/openshift-origin-bind-dns/config"
-    config.autoload_paths      += %W(#{config.root}/lib)
   end
 end
