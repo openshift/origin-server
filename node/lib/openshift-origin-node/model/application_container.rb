eeb89626ec6dd79a92a7de20b555bbe6030af441
Dan Mace <ironcladlou@gmail.com>
2012-11-29 09:55:22 -0500

    Refactor tidy into the node library
    The tidy operation is largely a platform responsibility. Move the
    core logic into the node library, and provide a cart level hook
    for any cart specific operations which must be performed.

--- node/lib/openshift-origin-node/model/application_container.rb
+++ node/lib/openshift-origin-node/model/application_container.rb
@@ -18,6 +18,7 @@ require 'rubygems'
 require 'openshift-origin-node/model/unix_user'
 require 'openshift-origin-node/utils/shell_exec'
 require 'openshift-origin-common'
+require 'logger'
 
 module OpenShift
   # == Application Container
@@ -37,7 +38,9 @@ module OpenShift
     end
 
     def initialize(application_uuid, container_uuid, user_uid = nil,
-        app_name = nil, container_name = nil, namespace = nil, quota_blocks = nil, quota_files = nil)
+        app_name = nil, container_name = nil, namespace = nil, quota_blocks = nil, quota_files = nil, logger = nil)
+      @logger = logger ||= Logger.new(STDOUT)
+
       @config = OpenShift::Config.new
 
       @uuid = container_uuid
@@ -152,6 +155,117 @@ module OpenShift
     def force_stop
       set_app_state(:STOPPED)
       UnixUser.kill_procs(@user.uid)
+    end
+
+
+    # Public: Cleans up the gear, providing any installed
+    # cartridges with the opportinity to perform their own
+    # cleanup operations via the tidy hook.
+    #
+    # The generic gear-level cleanup flow is:
+    # * Stop the gear
+    # * Git cleanup
+    # * Gear temp dir cleanup
+    # * Cartridge tidy hook executions
+    # * Start the gear
+    #
+    # Raises an Exception if an internal error occurs, and ignores
+    # failed cartridge tidy hook executions.
+    def tidy
+      @logger.debug("Starting tidy on gear #{@uuid}")
+
+      env = load_env
+      gear_dir = env[:OPENSHIFT_HOMEDIR]
+      app_name = env[:OPENSHIFT_APP_NAME]
+      gear_repo_dir = File.join(gear_dir, 'git', "#{app_name}.git")
+      gear_tmp_dir = File.join(gear_dir, '.tmp')
+
+      begin
+        # Stop the gear. If this fails, consider the tidy a failure.
+        out, err, rc = shellCmd("/usr/sbin/oo-admin-ctl-gears stopgear #{@user.uuid}", gear_dir, false, 0)
+        @logger.debug("Stopped gear #{@uuid}. Output:\n#{out}")
+      rescue OpenShift::Utils::ShellExecutionException => e
+        @logger.error(%Q{
+          Couldn't stop gear #{@uuid} for tidy: #{e.message}
+          --- stdout ---\n#{e.stdout}
+          --- stderr ---\n#{e.stderr}
+          })
+        raise "Tidy failed on gear #{@uuid}; the gear couldn't be stopped successfully"
+      end
+
+      # Perform the individual tidy actions. At this point, the gear has been stopped,
+      # and so we'll attempt to start the gear no matter what tidy operations fail.
+      begin
+        # Git pruning
+        tidy_action do
+          out, err, rc = shellCmd("git prune", gear_repo_dir, false, 0)
+          @logger.debug("Pruned git directory at #{gear_repo_dir}. Output:\n#{out}")
+        end
+
+        # Git GC
+        tidy_action do
+          out, err, rc = shellCmd("git gc --aggressive", gear_repo_dir, false, 0)
+          @logger.debug("Executed git gc for repo #{gear_repo_dir}. Output:\n#{out}")
+        end
+
+        # Temp dir cleanup
+        tidy_action do
+          FileUtils.rm_rf(Dir.glob(File.join(gear_tmp_dir, "*")))
+          @logger.debug("Cleaned gear temp dir at #{gear_tmp_dir}")
+        end
+
+        # Execute the tidy hooks in any installed carts, in the context
+        # of the gear user. For now, we detect cart installations by iterating
+        # over the gear subdirs and using the dir names to construct a path
+        # to cart scripts in the base cartridge directory. If such a file exists,
+        # it's assumed the cart is installed on the gear.
+        cart_tidy_timeout = 30
+        Dir.entries(gear_dir).each do |gear_subdir|
+          tidy_script = File.join(@config.get("CARTRIDGE_BASE_PATH"), gear_subdir, "info", "hooks", "tidy")
+          
+          next unless File.exists?(tidy_script)
+
+          begin
+            # Execute the hook in the context of the gear user
+            @logger.debug("Executing cart tidy script #{tidy_script} in gear #{@uuid} as user #{@user.uid}:#{@user.gid}")
+            OpenShift::Utils::ShellExec.run_as(@user.uid, @user.gid, tidy_script, gear_dir, false, 0, cart_tidy_timeout)
+          rescue OpenShift::Utils::ShellExecutionException => e
+            @logger.warn("Cartridge tidy operation failed on gear #{@uuid} for cart #{gear_dir}: #{e.message} (rc=#{e.rc})")
+          end
+        end
+      rescue Exception => e
+        @logger.warn("An unknown exception occured during tidy for gear #{@uuid}: #{e.message}\n#{e.backtrace}")
+      ensure
+        begin
+          # Start the gear, and if that fails raise an exception, as the app is now
+          # in a bad state.
+          out, err, rc = shellCmd("/usr/sbin/oo-admin-ctl-gears startgear #{@user.uuid}", gear_dir)
+          @logger.debug("Started gear #{@uuid}. Output:\n#{out}")
+        rescue OpenShift::Utils::ShellExecutionException => e
+          @logger.error(%Q{
+            Failed to restart gear #{@uuid} following tidy: #{e.message}
+            --- stdout ---\n#{e.stdout}
+            --- stderr ---\n#{e.stderr}
+            })
+          raise "Tidy of gear #{@uuid} failed, and the gear was not successfuly restarted"
+        end
+      end
+
+      @logger.debug("Completed tidy for gear #{@uuid}")      
+    end
+
+    # Executes a block, trapping ShellExecutionExceptions and treating them
+    # as warnings. Any other exceptions are unexpected and will bubble out.
+    def tidy_action
+      begin
+        yield
+      rescue OpenShift::Utils::ShellExecutionException => e
+        @logger.warn(%Q{
+          Tidy operation failed on gear #{@uuid}: #{e.message}
+          --- stdout ---\n#{e.stdout}
+          --- stderr ---\n#{e.stderr}
+          })
+      end
     end
 
     # Public: Load a gears environment variables into the environment
