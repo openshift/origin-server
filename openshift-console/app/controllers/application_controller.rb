1ad0d1d792395306b59a34ad7b6e7e89a35d041e
Chris Alfonso <calfonso@redhat.com>
2012-12-03 16:46:59 -0500

    BZ878754 No CSRF attack protection in console

--- openshift-console/app/controllers/application_controller.rb
+++ openshift-console/app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   include Console::Rescue
 
   protect_from_forgery
+
+  protected
+    def handle_unverified_request
+      raise Console::AccessDenied, "Request authenticity token does not match session #{session.inspect}"
+    end
 end
