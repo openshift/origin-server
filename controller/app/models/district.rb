28484a31002643a956fb2a8ffa21b37eee02f87d
Rajat Chopra <rchopra@redhat.com>
2012-12-14 16:10:26 -0800

    district search fix

--- controller/app/models/district.rb
+++ controller/app/models/district.rb
@@ -23,7 +23,7 @@ class District
   end
 
   def self.find_by_name(name)
-    return District.where(name: name)
+    return District.where(name: name)[0]
   end
   
   def initialize(attrs = nil, options = nil)
