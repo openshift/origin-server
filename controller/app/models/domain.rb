daf612843caa693421c8d69b5e251aeeaba4b6d8
Rajat Chopra <rchopra@redhat.com>
2013-01-02 16:48:17 -0800

    fix for bug#889986

--- controller/app/models/domain.rb
+++ controller/app/models/domain.rb
@@ -58,7 +58,7 @@ class Domain
   def update_namespace(new_namespace)
     old_ns = namespace
     set(:namespace, new_namespace)
-    pending_op = PendingDomainOps.new(op_type: :update_namespace, arguments: {"old_ns" => old_ns, "new_ns" => new_namespace}, parent_op: nil, on_apps: applications, on_complete_method: :complete_namespace_update, state: "init")
+    pending_op = PendingDomainOps.new(op_type: :update_namespace, arguments: {"old_ns" => old_ns, "new_ns" => new_namespace}, parent_op: nil, on_apps: applications, on_completion_method: :complete_namespace_update, state: "init")
     self.pending_ops.push pending_op
     self.run_jobs
     pending_op
