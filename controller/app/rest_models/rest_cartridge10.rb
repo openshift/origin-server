0d6e0646c80a13c9815739ae91a1bff0755dd2ab
Lili Nader <lnader@redhat.com>
2013-01-04 21:24:01 -0800

    fixed missing attribute

--- controller/app/rest_models/rest_cartridge10.rb
+++ controller/app/rest_models/rest_cartridge10.rb
@@ -1,11 +1,11 @@
 class RestCartridge10 < OpenShift::Model
-  attr_accessor :type, :name, :properties
+  attr_accessor :type, :name, :properties, :status_messages
 
   def initialize(cart)
     self.name = cart.name
     self.type = "standalone"
     self.type = "embedded" if cart.categories.include? "embedded"
-
+    self.status_messages = nil
     self.properties = {}
   end
 
