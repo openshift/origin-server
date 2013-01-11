3a4475b77ed33c1e39dc0c3f68ca71392fdeb412
Fabiano Franz <ffranz@redhat.com>
2013-01-11 17:21:43 -0200

    Added simple OAuth client support as a mixin

--- console/app/models/rest_api/base.rb
+++ console/app/models/rest_api/base.rb
@@ -5,6 +5,30 @@ require 'active_resource/reflection'
 require 'active_support/concern'
 require 'active_model/dirty'
 require 'active_resource/persistent_connection'
+require 'net/http'
+
+module Net
+  class HTTP
+    def self.enable_debug!
+      raise "You don't want to do this in anything but development mode!" unless Rails.env == 'development'
+      class << self
+        alias_method :__new__, :new
+        def new(*args, &blk)
+          instance = __new__(*args, &blk)
+          instance.set_debug_output($stderr)
+          instance
+        end
+      end
+    end
+
+    def self.disable_debug!
+      class << self
+        alias_method :new, :__new__
+        remove_method :__new__
+      end
+    end
+  end
+end
 
 class ActiveResource::Connection
   #
---
+++ console/app/models/rest_api/oauth.rb
@@ -0,0 +1,72 @@
+require 'uri'
+require 'cgi'
+require 'openssl'
+require 'base64'
+
+module RestApi
+  module Oauth
+    extend ActiveSupport::Concern
+
+    module ClassMethods
+
+      SIGNATURE_METHOD = "HMAC-SHA1"
+      OAUTH_VERSION = "1.0"
+      CONTENT_TYPE = 'application/x-www-form-urlencoded'
+      METHOD = 'GET'
+
+      attr_reader :oauth_endpoint_uri, :oauth_consumer_key, :oauth_consumer_secret, :oauth_token, :oauth_token_secret, :oauth_nonce
+
+      def oauth(endpoint_url, consumer_key, consumer_secret, token, token_secret)
+        @oauth_endpoint_uri = URI(endpoint_url)
+        @oauth_consumer_key, @oauth_consumer_secret, @oauth_token, @oauth_token_secret = consumer_key, consumer_secret, token, token_secret
+        @oauth_nonce = create_oauth_nonce
+        headers['Content-Type'] = CONTENT_TYPE
+        headers['Authorization'] = oauth_authorization_header
+      end
+
+      private
+        def oauth_parameters
+          @oauth_parameters ||= {
+            'oauth_consumer_key' => @oauth_consumer_key,
+            'oauth_nonce' => @oauth_nonce,
+            'oauth_signature_method' => SIGNATURE_METHOD,
+            'oauth_timestamp' => timestamp,
+            'oauth_token' => @oauth_token,
+            'oauth_version' => OAUTH_VERSION
+          }
+        end
+
+        def oauth_authorization_header
+          'OAuth ' + 
+          oauth_parameters.sort.map{|k,v| "#{percent_encode(k)}=\"#{percent_encode(v)}\""}.join(', ') + 
+          ', ' + 
+          "oauth_signature=\"#{percent_encode(oauth_signature)}\""
+        end
+
+        def create_oauth_nonce
+          Array.new(5) { rand(256) }.pack('C*').unpack('H*').first
+        end
+
+        def oauth_signature
+          signing_key = percent_encode(@oauth_consumer_secret) + '&' + percent_encode(@oauth_token_secret)
+          query_string = CGI::parse(@oauth_endpoint_uri.query) rescue {}
+          signature_base_string = [
+            METHOD, 
+            percent_encode(@oauth_endpoint_uri.scheme + "://" + @oauth_endpoint_uri.host + @oauth_endpoint_uri.path), 
+            percent_encode(oauth_parameters.merge(query_string).sort.map{|k,v| "#{percent_encode(k)}=#{percent_encode(v.kind_of?(Array) ? v.first : v)}"}.join('&'))
+          ].join('&')
+          digest = OpenSSL::Digest::Digest.new('sha1')
+          hmac = OpenSSL::HMAC.digest(digest, signing_key, signature_base_string)
+          Base64.encode64(hmac).chomp.gsub(/\n/, '')
+        end
+
+        def timestamp
+          Time.now.to_i.to_s
+        end
+
+        def percent_encode(string)
+          URI.escape(string, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]")).gsub('*', '%2A')
+        end
+    end
+  end
+end
