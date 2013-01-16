88d5c0aa3bf5d860247b2003546436c23ced287c
Abhishek Gupta <abhgupta@redhat.com>
2013-01-15 16:13:17 -0800

    fixing broker integration tests

--- broker/test/integration/cloud_user_test.rb
+++ broker/test/integration/cloud_user_test.rb
@@ -4,20 +4,56 @@ require 'mocha'
 
 class CloudUserTest < ActiveSupport::TestCase
   
-  test "create" do
+  test "create cloud user" do
     login = "user_" + gen_uuid
     orig_cu = CloudUser.new(login: login)
-    orig_cu.save
+    orig_cu.save!
     cu = CloudUser.find_by(login: login)
     assert_equal_users(orig_cu, cu)
   end
   
-  test "find by uuid" do
+  test "find by id" do
     login = "user_" + gen_uuid
     orig_cu = CloudUser.new(login: login)
-    orig_cu.save
+    orig_cu.save!
     cu = CloudUser.find(orig_cu._id)
     assert_equal_users(orig_cu, cu)
+  end
+
+  test "delete cloud user" do
+    login = "user_" + gen_uuid
+    orig_cu = CloudUser.new(login: login)
+    orig_cu.save!
+    orig_cu.delete
+    
+    cu = nil
+    begin
+      cu = CloudUser.find_by(login: login)
+    rescue Mongoid::Errors::DocumentNotFound
+      # do nothing
+    end
+
+    assert_equal(nil, cu)
+  end
+  
+  test "update cloud user" do
+    login = "user_" + gen_uuid
+    orig_cu = CloudUser.new(login: login)
+    orig_cu.save!
+
+    orig_cu.set(:consumed_gears, 2)
+    
+    updated_cu = CloudUser.find_by(login: login)
+    assert_equal(orig_cu.consumed_gears, updated_cu.consumed_gears)
+  end
+  
+  test "find all cloud users" do
+    part_login = gen_uuid
+    ["_user1", "_user2"].each do |username|
+      orig_cu = CloudUser.new(login: part_login + username)
+      orig_cu.save!
+    end
+    assert_equal(CloudUser.where(login: /#{part_login}/i).count, 2)
   end
   
   def assert_equal_users(user1, user2)
--- broker/test/integration/domain_test.rb
+++ broker/test/integration/domain_test.rb
@@ -8,12 +8,41 @@ class DomainTest < ActiveSupport::TestCase
     OpenShift::DnsService.provider=OpenShift::DnsService
   end
 
-  test "create" do
+  test "create and find domain" do
     ns = "ns" + gen_uuid[0..12]
     orig_d = Domain.new(namespace: ns, canonical_namespace: ns.downcase)
     orig_d.save!
     d = Domain.find_by(canonical_namespace: ns.downcase)
     assert_equal_domains(orig_d, d)
+  end
+
+  test "delete cloud domain" do
+    ns = "ns" + gen_uuid[0..12]
+    orig_d = Domain.new(namespace: ns, canonical_namespace: ns.downcase)
+    orig_d.save!
+
+    orig_d.delete
+
+    d = nil
+    begin
+      d = Domain.find_by(canonical_namespace: ns.downcase)
+    rescue Mongoid::Errors::DocumentNotFound
+      # do nothing
+    end
+    
+    assert_equal(nil, d)
+  end
+
+  test "update domain" do
+    ns = "ns" + gen_uuid[0..9]
+    orig_d = Domain.new(namespace: ns, canonical_namespace: ns.downcase)
+    orig_d.save!
+
+    orig_d.set(:canonical_namespace, ns.downcase + "new")
+    orig_d.set(:namespace, ns + "new")
+
+    new_d = Domain.find_by(canonical_namespace: ns.downcase + "new")
+    assert_equal_domains(orig_d, new_d)
   end
   
   def assert_equal_domains(domain1, domain2)
