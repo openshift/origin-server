4fa440715651ba37283ee1430f0a0f8252515b20
Abhishek Gupta <abhgupta@redhat.com>
2013-01-23 18:22:46 -0800

    adding a dash in the authorized key entry comment to make it more readable

--- node/lib/openshift-origin-node/model/unix_user.rb
+++ node/lib/openshift-origin-node/model/unix_user.rb
@@ -258,7 +258,7 @@ Dir(after)    #{@uuid}/#{@uid} => #{list_home_dir(@homedir)}
       keys = read_ssh_keys authorized_keys_file
       key_type    = "ssh-rsa" if key_type.to_s.strip.length == 0
       cloud_name  = @config.get("CLOUD_NAME") || "OPENSHIFT"
-      ssh_comment = "#{cloud_name}-#{@uuid}#{comment}"
+      ssh_comment = "#{cloud_name}-#{@uuid}-#{comment}"
       shell       = @config.get("GEAR_SHELL") || "/bin/bash"
       cmd_entry   = "command=\"#{shell}\",no-X11-forwarding #{key_type} #{key} #{ssh_comment}"
 
