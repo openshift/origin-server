39b5d1220ea59ccfbb2ac6010f46a7ef03c7112e
Steve Goodwin <sgoodwin@redhat.com>
2013-02-07 13:58:49 -0500

    Add function for placeholder on create app form include jquery_placeholder for ie9 and older

--- console/app/assets/javascripts/console.js
+++ console/app/assets/javascripts/console.js
@@ -2,6 +2,7 @@
 //= require jquery_ujs
 //= require jquery_validate_min
 //= require jquery_cookie
+//= require jquery_placeholder
 //= require plugins
 //= require bootstrap-collapse
 //= require bootstrap-transition
