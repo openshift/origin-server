d6763549b49a11937f7b6bbff84f1583fa786414
Michael Scherer <misc@zarb.org>
2013-03-03 17:06:34 +0100

    Update code for default server.js so no warning are displayed
    When running with a newest express module, this
    message is shown :
    
      Warning: express.createServer() is deprecated, express
      applications no longer inherit from http.Server,
      please use:
    
        var express = require("express");
        var app = express();

--- cartridges/openshift-origin-cartridge-nodejs-0.6/template/server.js
+++ cartridges/openshift-origin-cartridge-nodejs-0.6/template/server.js
@@ -118,7 +118,7 @@ var SampleApp = function() {
      */
     self.initializeServer = function() {
         self.createRoutes();
-        self.app = express.createServer();
+        self.app = express();
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
