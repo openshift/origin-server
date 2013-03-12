49a7aef74d8724198cd3f9f8cc6785b440010821
Clayton Coleman <ccoleman@redhat.com>
2013-03-12 16:08:55 -0400

    Updated date helpers

--- console/config/initializers/date_helper.rb
+++ console/config/initializers/date_helper.rb
@@ -1,5 +1,14 @@
 Time::DATE_FORMATS.merge!({
   :pretty_date => lambda do |time| 
+    time.strftime("%B #{ActiveSupport::Inflector.ordinalize(time.day)}")
+  end,
+  :billing_date => lambda do |time| 
+    time.strftime("%B #{ActiveSupport::Inflector.ordinalize(time.day)}, %Y")
+  end,
+  :credit_card => lambda do |time| 
+    time.strftime("%B %Y")
+  end,
+  :pretty_time => lambda do |time| 
     time.strftime("%A, %b #{ActiveSupport::Inflector.ordinalize(time.day)}")
   end,
 })
