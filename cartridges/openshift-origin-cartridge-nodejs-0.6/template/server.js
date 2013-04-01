c82886db20c9ac574d7cebf57558fa72d0f3703f
Dan McPherson <dmcphers@redhat.com>
2013-04-01 19:01:16 -0400

    Revert "Update code for default server.js so no warning are displayed"
    This reverts commit d6763549b49a11937f7b6bbff84f1583fa786414.

--- cartridges/openshift-origin-cartridge-nodejs-0.6/template/server.js
+++ cartridges/openshift-origin-cartridge-nodejs-0.6/template/server.js
@@ -118,7 +118,7 @@ var SampleApp = function() {
      */
     self.initializeServer = function() {
         self.createRoutes();
-        self.app = express();
+        self.app = express.createServer();
 
         //  Add handlers for the app (from the routes).
         for (var r in self.routes) {
