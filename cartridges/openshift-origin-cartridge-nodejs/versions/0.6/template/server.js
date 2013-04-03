59a4fa63e2b12f584a6800f2bac91fb6435f09b7
Hiro Asari <asari.ruby@gmail.com>
2013-04-03 11:21:18 -0400

    setup and control now work together to create a Node.js app that runs.
    In 'setup', 'npm link' actually yields error, which will be logged to $OPENSHIFT_NODEJS_DIR/npm-debug.log,
    but this does not prevent the applciation from starting and listening on port $OPENSHIFT_NODEJS_PORT.
    As a result of this error, however, '-e' is removed from the shebang line, and 'exit 0' is explicitly specified.

--- cartridges/openshift-origin-cartridge-nodejs/versions/0.6/template/server.js
+++ cartridges/openshift-origin-cartridge-nodejs/versions/0.6/template/server.js
@@ -22,13 +22,13 @@ var SampleApp = function() {
      */
     self.setupVariables = function() {
         //  Set the environment variables we need.
-        self.ipaddress = process.env.OPENSHIFT_INTERNAL_IP;
-        self.port      = process.env.OPENSHIFT_INTERNAL_PORT || 8080;
+        self.ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+        self.port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
         if (typeof self.ipaddress === "undefined") {
             //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
             //  allows us to run/test the app locally.
-            console.warn('No OPENSHIFT_INTERNAL_IP var, using 127.0.0.1');
+            console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
             self.ipaddress = "127.0.0.1";
         };
     };
