1e3a6a0ca4083e4338b17c898c29229398709671
Lili Nader <lnader@redhat.com>
2013-04-02 17:38:19 -0700

    Part 1 of Card 536

--- controller/test/cucumber/cartridge-lifecycle-php.feature
+++ controller/test/cucumber/cartridge-lifecycle-php.feature
@@ -41,7 +41,7 @@ Feature: Cartridge Lifecycle PHP Verification Tests
   
   @rhel-only
   Scenario: Application Change Namespace  (RHEL/CentOS)
-    Given an existing php-5.3 application, verify its namespace can be changed
+    Given an existing php-5.3 application, verify its namespace cannot be changed
     
   @rhel-only
   Scenario: Application Destroying  (RHEL/CentOS)
@@ -87,7 +87,7 @@ Feature: Cartridge Lifecycle PHP Verification Tests
   
   @fedora-only
   Scenario: Application Change Namespace  (RHEL/CentOS)
-    Given an existing php-5.4 application, verify its namespace can be changed
+    Given an existing php-5.4 application, verify its namespace cannot be changed
   
   @fedora-only
   Scenario: Application Destroying  (RHEL/CentOS)
--- controller/test/cucumber/support/command_helper.rb
+++ controller/test/cucumber/support/command_helper.rb
@@ -137,31 +137,34 @@ module CommandHelper
   end
 
   def rhc_update_namespace(app)
+    ########### Note: the update of application namespace is no longer supported ##################
     rhc_do('rhc_update_namespace') do
       old_namespace = app.namespace
       if old_namespace.end_with?('new')
-        app.namespace = new_namespace = old_namespace[0..-4]
+        #app.namespace = new_namespace = old_namespace[0..-4]
+        new_namespace = old_namespace[0..-4]
       else
-        app.namespace = new_namespace = old_namespace + "new"
+        #app.namespace = new_namespace = old_namespace + "new"
+        new_namespace = old_namespace + "new"
       end
-      old_hostname = app.hostname
-      app.hostname = "#{app.name}-#{new_namespace}.#{$domain}"
-      old_repo = app.repo
-      app.repo = "#{$temp}/#{new_namespace}_#{app.name}_repo"
-      FileUtils.mv old_repo, app.repo
+      #old_hostname = app.hostname
+      #app.hostname = "#{app.name}-#{new_namespace}.#{$domain}"
+      #old_repo = app.repo
+      #app.repo = "#{$temp}/#{new_namespace}_#{app.name}_repo"
+      #FileUtils.mv old_repo, app.repo
       
-      if run("grep '#{old_hostname}' #{app.repo}/.git/config") == 0
-        run("sed -i 's,#{old_hostname},#{app.hostname},g' #{app.repo}/.git/config")
-      end
+      #if run("grep '#{old_hostname}' #{app.repo}/.git/config") == 0
+      #  run("sed -i 's,#{old_hostname},#{app.hostname},g' #{app.repo}/.git/config")
+      #end
       
-      if run("grep '#{app.name}-#{old_namespace}.#{$domain}' /etc/hosts") == 0
-        run("sed -i 's,#{app.name}-#{old_namespace}.#{$domain},#{app.name}-#{new_namespace}.#{$domain},g' /etc/hosts")
-      end
-      old_file = app.file
-      app.file = "#{$temp}/#{new_namespace}.json"
-      FileUtils.mv old_file, app.file
+      #if run("grep '#{app.name}-#{old_namespace}.#{$domain}' /etc/hosts") == 0
+      #  run("sed -i 's,#{app.name}-#{old_namespace}.#{$domain},#{app.name}-#{new_namespace}.#{$domain},g' /etc/hosts")
+      #end
+      #old_file = app.file
+      #app.file = "#{$temp}/#{new_namespace}.json"
+      #FileUtils.mv old_file, app.file
       time = Benchmark.realtime do 
-        run("#{$rhc_script} domain update #{old_namespace} #{new_namespace} #{default_args(app)}").should == 0
+        run("#{$rhc_script} domain update #{old_namespace} #{new_namespace} #{default_args(app)}").should == 1
       end
       log_event "#{time} UPDATE_DOMAIN #{new_namespace} #{app.login}"
       app.persist
