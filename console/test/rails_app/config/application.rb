ea7e245a5a3c1733464727e22935f053fa99d0fc
Brenton Leanhardt <bleanhar@redhat.com>
2013-05-06 09:47:36 -0400

    We don't want test dependencies required at runtime
    Test depedencies that make their way into the spec files result in wasted time
    for distro maintainers.  Our tests are not run at RPM build time nor can they
    run in brew or koji.
    
    By default the Gemfile in git will be configured for development.  At RPM build
    time a platform specific runtime Gemfile will be installed.
    
    Along with this solution is the addition of openshift-origin-devel.  That
    package includes all the test dependencies.  This will be installed by
    origin-dev-tools' devenv script however there is no expectation that distro
    maintainers ship this new package.

--- console/test/rails_app/config/application.rb
+++ console/test/rails_app/config/application.rb
@@ -3,7 +3,6 @@ require File.expand_path('../boot', __FILE__)
 require "action_controller/railtie"
 require "action_mailer/railtie"
 require "active_resource/railtie"
-require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups(:default, :assets => %w(development test))) if defined?(Bundler)
 require "console/engine"
