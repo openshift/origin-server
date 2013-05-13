71131b969462c4464b9b728d8b3e04cd8e26abd4
Justin Harris <jharris@redhat.com>
2013-05-13 16:00:29 -0500

    Card online_ui_278 - Log helper utility

---
+++ console/app/helpers/log_helper.rb
@@ -0,0 +1,20 @@
+module LogHelper
+
+  # Common - request id, user agent, ip address
+  def user_action(action, success=true, options={}, message=nil)
+    action_info = {
+      :user_agent => request.user_agent,
+      :ip_address => request.remote_ip,
+      :request_id => request.uuid
+    }.merge(options)
+
+    status = success ? 'SUCCESS' : 'FAILURE'
+    now = Time.new
+    date = now.strftime('%Y-%m-%d')
+    time = now.strftime('%H:%M:%S')
+
+    optional_params = action_info.map { |k,v| "#{k.upcase}=#{v}" }.join(' ')
+    Rails.logger.info("[user_action] #{status} DATE=#{date} TIME=#{time} ACTION=#{action.upcase} #{optional_params} #{message || ''}".strip)
+  end
+
+end
