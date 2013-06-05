1d708f6f701fa2b53a8975438c4ad3111085caae
Tim Bielawa <tbielawa@redhat.com>
2013-06-04 22:44:40 -0400

    <App creation error message> Clarify wording in error message
    https://github.com/openshift/origin-server/issues/2756
    
    Fix up language in the 'Invalid size' error message when selecting an
    invalid gear size. Also clean up some trailing witespace. Related to

--- controller/app/controllers/applications_controller.rb
+++ controller/app/controllers/applications_controller.rb
@@ -7,7 +7,7 @@ class ApplicationsController < BaseController
   before_filter :get_application, :only => [:show, :destroy, :update]
   ##
   # List all applications
-  # 
+  #
   # URL: /domains/:domain_id/applications
   # @param [String] include Comma separated list of sub-objects to include in reply. Only "cartridges" is supported at the moment.
   #
@@ -27,7 +27,7 @@ class ApplicationsController < BaseController
 
   ##
   # Retrieve a specific application
-  # 
+  #
   # URL: /domains/:domain_id/applications/:id
   #
   # Action: GET
@@ -39,7 +39,7 @@ class ApplicationsController < BaseController
 
   ##
   # Create a new application
-  # 
+  #
   # URL: /domains/:domain_id/applications
   #
   # Action: POST
@@ -55,8 +55,8 @@ class ApplicationsController < BaseController
     features = []
     downloaded_cart_urls = []
     cart_params = [(params[:cartridges].presence || params[:cartridge].presence)].flatten
-    cart_params.each do |c| 
-      if c.is_a?(Hash) 
+    cart_params.each do |c|
+      if c.is_a?(Hash)
         if c[:name]
           features << c[:name]
         elsif c[:url]
@@ -64,8 +64,8 @@ class ApplicationsController < BaseController
         end
       else
         features << c
-      end  
-    end 
+      end
+    end
     init_git_url = params[:initial_git_url].presence
 
     return render_error(:unprocessable_entity, "Invalid initial git URL",
@@ -78,7 +78,7 @@ class ApplicationsController < BaseController
                         105, "name") if !app_name or app_name.empty?
 
     valid_sizes = OpenShift::ApplicationContainerProxy.valid_gear_sizes(@domain.owner)
-    return render_error(:unprocessable_entity, "Invalid size: #{default_gear_size}. Acceptable values are #{valid_sizes.join(",")}",
+    return render_error(:unprocessable_entity, "Invalid size: #{default_gear_size}. Acceptable values are: #{valid_sizes.join(",")}",
                         134, "gear_profile") if default_gear_size and !valid_sizes.include?(default_gear_size)
 
     if Application.where(domain: @domain, canonical_name: app_name.downcase).count > 0
@@ -112,7 +112,7 @@ class ApplicationsController < BaseController
     rescue OpenShift::UserException => e
       return render_error(:unprocessable_entity, e.message, e.code, e.field)
     rescue Exception => e
-      return render_exception(e)  
+      return render_exception(e)
     end
     application.user_agent= request.headers['User-Agent']
 
@@ -124,7 +124,7 @@ class ApplicationsController < BaseController
 
   ##
   # Delete an application
-  # 
+  #
   # URL: /domains/:domain_id/applications/:id
   #
   # Action: DELETE
@@ -139,7 +139,7 @@ class ApplicationsController < BaseController
     end
 
     status = requested_api_version <= 1.4 ? :no_content : :ok
-    return render_success(status, nil, nil, "Application #{id} is deleted.", result) 
+    return render_success(status, nil, nil, "Application #{id} is deleted.", result)
   end
 
   def set_log_tag
