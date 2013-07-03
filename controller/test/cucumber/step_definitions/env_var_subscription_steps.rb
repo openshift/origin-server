257001ab734b8c4a137fb01ee286f0a71423c164
dobbymoodge <jolamb@redhat.com>
2013-07-03 17:08:39 -0400

    <application.rb> Add feature to carts to handle wildcard ENV variable subscriptions
    https://trello.com/c/B0BXoIjK/160-5-option-to-get-all-published-env-vars-carteco
    
     * Created new method - subscription_filter - to resolve
       subscriptions for component_instances without altering the
       cartridge objects themselves. This method
       handles "traditional" environment variable subscription
       and "wildcard" ENV:* subscription.
    
     * Added logic to elaborate method to consume new subscriptions
       array
    
     * Updated carts to use new wildcard environment variable
       subscription where appropriate
    
     * Added tests to verify both wildcard and traditional
       subscription specification in cart manifest is handled
       properly
    
     * Added logic to cart_repo_helper.rb to clean up both mock and
       mock-plugin carts
    
     * Added two unit tests to exercise elaborate() and
       subscription_filter() for cases where environment variables are
       subscribed to via "ENV:*" and where they are subscribed via
       connection type specification
       (i.e. "ENV:NET_TCP:db:connection-info")

---
+++ controller/test/cucumber/step_definitions/env_var_subscription_steps.rb
@@ -0,0 +1,59 @@
+Given /^a version of the ([^ ]+)\-([\d\.]+) cartridge with additional published ENV vars is installed$/ do |cart_name, component_version|
+  tmp_cart_src = "/tmp/#{cart_name}-cucumber-rewrite/publish"
+  # Add a new hook that publishes an unique marker variable we can then check for
+  garbage_hook_content = <<-EOF
+#!/bin/bash
+echo OPENSHIFT_MOCK_PLUGIN_GARBAGE=junk
+EOF
+  new_hooks = [ { :name => "publish-garbage-info",
+                  :content => garbage_hook_content } ]
+
+  current_manifest = prepare_cart_for_rewrite(tmp_cart_src, cart_name, component_version)
+  rewrite_and_install(current_manifest, tmp_cart_src, new_hooks) do |manifest, current_version|
+    # Advertise unique marker in publishing manifest
+    manifest['Publishes']['publish-garbage-info'] = {'Type' => 'ENV:NET_TCP:garbage:info'}
+    manifest['Compatible-Versions'] = [ current_version ]
+  end
+
+end
+
+Given /^a version of the ([^ ]+)\-([\d\.]+) cartridge with(out)? wildcard ENV subscription is installed$/ do |cart_name, component_version, negate|
+  tmp_cart_src = "/tmp/#{cart_name}-cucumber-rewrite/#{negate ? 'dont' : 'do'}-publish"
+  current_manifest = prepare_cart_for_rewrite(tmp_cart_src, cart_name, component_version)
+  rewrite_and_install(current_manifest, tmp_cart_src) do |manifest, current_version|
+    manifest['Compatible-Versions'] = [ current_version ]
+    if negate
+      # Remove wildcard ENV var subscription if it exists
+      manifest['Subscribes'].delete_if do |connection, params|
+        params['Type'] == 'ENV:*'
+      end
+      # Add targeted ENV var subscriptions
+      manifest['Subscribes'] = {
+        'set-db-connection-info' => {
+          'Type' => "ENV:NET_TCP:db:connection-info",
+          'Required' => false
+        },
+        'set-nosql-db-connection-info' => {
+          'Type' => "ENV:NET_TCP:nosqldb:connection-info",
+          'Required' => false
+        } }
+    else
+      # Remove any targeted ENV var subscriptions
+      manifest['Subscribes'].delete_if do |connection, params|
+        params['Type'].start_with? 'ENV:'
+      end
+      # Add wildcard ENV var subscription
+      manifest['Subscribes']['set-env'] = { 'Type' => 'ENV:*', 'Required' => 'false' }
+    end
+  end
+end
+
+Given /^the broker cache is cleared$/ do
+  %x(/usr/sbin/oo-admin-broker-cache --clear)
+end
+
+Then /^the ([^ ]+) application environment variable ([^ ]+) will( not)? exist$/ do |cart_name, cart_var, negate|
+  var_name = "OPENSHIFT_#{cart_var}"
+  var_file_path = File.join($home_root, @app.uid, '.env', cart_name, var_name)
+  check_var_name(var_file_path, nil, negate)
+end
