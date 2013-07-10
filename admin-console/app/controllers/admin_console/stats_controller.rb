b8acaf53b7ad9c9d9141a0ebc8e3ba71fc6741de
Jessica Forrester <jforrest@redhat.com>
2013-07-10 18:30:51 -0400

    Pull statistics generation into models

--- admin-console/app/controllers/admin_console/stats_controller.rb
+++ admin-console/app/controllers/admin_console/stats_controller.rb
@@ -5,52 +5,19 @@ module AdminConsole
     respond_to :json
 
     def index
-      @stats = {
-        :application => {
-          :total => Application.count
-        },
-        :user => {
-          :total => CloudUser.count
-        }
-        #TODO node
-      }
+      @stats = AdminConsole::Stats.collect
     end
 
     # rest api that collects the advanced, long-running statistics
-    def advanced
-      gears_per_user = CloudUser.collection.aggregate(
-        {
-           "$project" => {
-              "consumed_bin" => { "$cond" => [{"$lt" => ["$consumed_gears",100]},"$consumed_gears",100]}
-           }
-        },
-        {
-           "$group" => {
-              "_id" => "$consumed_bin",
-              "count" => { "$sum" => 1 }
-           }
-        })
-
-      apps_per_domain = Application.collection.aggregate(
-        {
-           "$group" => {
-              "_id" => "$domain_id",
-              "count" => { "$sum" => 1 }
-           }
-        },
-        {
-           "$project" => {
-              "app_per_domain_bin" => { "$cond" => [{"$lt" => ["$count",100]},"$count",100]}
-           }
-        },
-        {
-           "$group" => {
-              "_id" => "$app_per_domain_bin",
-              "count" => { "$sum" => 1 }
-           }
-        })
-
-      respond_with({:apps_per_domain => apps_per_domain, :gears_per_user => gears_per_user})
+    def show
+      case params[:id]
+      when "gears_per_user"
+        respond_with CloudUserStats.gears_per_user_binning
+      when "apps_per_domain"
+        respond_with ApplicationStats.apps_per_domain_binning
+      else
+        #TODO respond with 404
+      end
     end
   end
 end
