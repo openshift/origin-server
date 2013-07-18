54d8c94904a3ec48c873d01fe1378b2de13fa58f
Dan Mace <ironcladlou@gmail.com>
2013-07-18 09:04:53 -0400

    Upgrade enhancements
    Refactor the upgrade code to use well-defined step and itinerary structures. Return a JSON payload of
    all relevant upgrade data to facilitate enhancements in higher level upgrade orchestration tools.
    
    Add a new '--rerun' feature to oo-admin-upgrade which allows the re-upgrade of the last run's errors,
    facilitating an "upgrade until it succeeds" workflow.

---
+++ node/lib/openshift-origin-node/utils/upgrade_itinerary.rb
@@ -0,0 +1,55 @@
+module OpenShift
+  module Runtime
+    module UpgradeType
+      COMPATIBLE   = 'compatible'
+      INCOMPATIBLE = 'incompatible'
+    end
+
+  	class UpgradeItinerary
+      attr_reader :entries
+
+  	  def initialize(gear_home, entries = {}, has_incompatible = false)
+  	  	@gear_home = gear_home
+        @entries = entries || {}
+        @has_incompatible = has_incompatible
+  	  end
+
+      def create_entry(cartridge_name, upgrade_type)
+      	@entries[cartridge_name] = upgrade_type
+
+      	if upgrade_type == UpgradeType::INCOMPATIBLE
+      	  @has_incompatible = true
+      	end
+      end
+
+      def has_incompatible_upgrade?
+      	@has_incompatible
+      end
+
+      def each_cartridge
+        @entries.each_pair do |name, upgrade_type|
+          yield name, upgrade_type if block_given?
+        end
+      end
+
+      def persist
+      	jsonish_self = { entries: @entries, has_incompatible: @has_incompatible}
+        IO.write(self.class.itinerary_file(@gear_home), JSON.dump(jsonish_self))
+      end
+
+      def self.itinerary_file(gear_home)
+        PathUtils.join(gear_home, %w(app-root runtime .upgrade_itinerary))
+      end
+
+      def self.for_gear(gear_home)
+        serialized_self = IO.read(itinerary_file(gear_home))
+        jsonish_self = JSON.load(serialized_self)
+        UpgradeItinerary.new(gear_home, jsonish_self['entries'], jsonish_self['has_incompatible'])
+      end
+
+      def self.remove_from(gear_home)
+        FileUtils.rm_f(itinerary_file(gear_home))
+      end
+  	end
+  end
+end
