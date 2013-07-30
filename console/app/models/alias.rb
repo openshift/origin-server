672edbaca33aa8ddbecb4b498b19fb7dab4078da
fabianofranz <ffranz@redhat.com>
2013-07-30 17:56:12 -0300

    Bug 985952 - should not touch certificate if chain was not provided

--- console/app/models/alias.rb
+++ console/app/models/alias.rb
@@ -47,8 +47,11 @@ class Alias < RestApi::Base
 
   def recreate_ssl_certificate
     if !@certificate_file.nil?
-      self.ssl_certificate = @certificate_file.strip
-      self.ssl_certificate << "\n" << @certificate_chain_file unless @certificate_chain_file.nil?
+      self.ssl_certificate = @certificate_file
+      if !@certificate_chain_file.nil?
+        self.ssl_certificate.strip!
+        self.ssl_certificate << "\n" << @certificate_chain_file
+      end
     end
   end
 
