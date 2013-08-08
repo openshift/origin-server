c14f98d9c7c3dddef84e1d53854eba3fe9b08637
Ravi Sankar <rpenta@redhat.com>
2013-08-07 18:04:21 -0700

    User vars node changes:  - Use 'user-var-add' mcollective call for *add* and/or *push* user vars. This will reduce unnecessary additional code/complexity.  - Add some more reserved var names: PATH, IFS, USER, SHELL, HOSTNAME, LOGNAME  - Do not attempt rsync when .env/user_vars dir is empty  - Misc bug fixes

--- node/lib/openshift-origin-node/model/application_container_ext/environment.rb
+++ node/lib/openshift-origin-node/model/application_container_ext/environment.rb
@@ -5,6 +5,10 @@ module OpenShift
 
         USER_VARIABLE_MAX_COUNT = 25
         USER_VARIABLE_MAX_SIZE = 512
+        RESERVED_VARIABLE_NAMES = [
+                                   'OPENSHIFT_PRIMARY_CARTRIDGE_DIR', 'OPENSHIFT_NAMESPACE', 'PATH',
+                                   'IFS', 'USER', 'SHELL', 'HOSTNAME', 'LOGNAME'
+                                  ]
 
         # Public: Add an environment variable to a given gear.
         #
@@ -269,8 +273,7 @@ module OpenShift
 
             if File.exists?(path) ||
                 name =~ /\AOPENSHIFT_.*_IDENT\Z/ ||
-                name == 'OPENSHIFT_PRIMARY_CARTRIDGE_DIR' ||
-                name == 'OPENSHIFT_NAMESPACE'
+                RESERVED_VARIABLE_NAMES.include?(name)
               return 127, "CLIENT_ERROR: #{name} cannot be overridden"
             end
 
@@ -308,7 +311,7 @@ module OpenShift
           output, gear_dns, threads = '', '', {}
           target  = PathUtils.join('.env', 'user_vars').freeze
           source  = PathUtils.join(@container_dir, target).freeze
-          return 0, '' unless File.directory?(source)
+          return 0, '' unless File.directory?(source) and !(Dir.entries(source) - %w{. ..}).empty?
 
           begin
             gears.each do |gear|
@@ -317,12 +320,14 @@ module OpenShift
                 gear_dns = fqdn
                 retries  = 2
                 begin
-                  command = "/usr/bin/rsync -arvO --delete -e 'ssh -i #{@container_dir}/.openshift_ssh/id_rsa' #{source}/ #{fqdn}:#{target}"
-                  ::OpenShift::Runtime::Utils::oo_spawn(command, expected_exitstatus: 0)
+                  command = "/usr/bin/rsync -arvO --delete -e ssh #{source}/ #{fqdn}:#{target}"
+                  env = OpenShift::Runtime::Utils::Environ.for_gear(Etc.getpwnam(@uuid).dir)
+                  ::OpenShift::Runtime::Utils::oo_spawn(command, expected_exitstatus: 0, uid: @uid, env: env)
                 rescue Exception => e
                   NodeLogger.logger.debug { "Push #{retries} #{source} exception #{e.message}" }
                   Thread.current[:exception] = e
                   retries                    -= 1
+                  sleep(0.5)
                   retry if 0 < retries
                 end
               end
