9d86272aa4ad8491fd02575a957fceb1afb1e46e
Clayton Coleman <ccoleman@redhat.com>
2013-08-16 10:35:02 -0400

    Bug 997831 - Domain scope not in Rails config

--- broker/config/environments/production.rb
+++ broker/config/environments/production.rb
@@ -76,7 +76,7 @@ Broker::Application.configure do
     :gear_sizes => conf.get("VALID_GEAR_SIZES", "small").split(","),
     :default_gear_capabilities => conf.get("DEFAULT_GEAR_CAPABILITIES", "small").split(","),
     :community_quickstarts_url => conf.get("COMMUNITY_QUICKSTARTS_URL"),
-    :scopes => ['Scope::Session', 'Scope::Read', 'Scope::Application', 'Scope::Userinfo'],
+    :scopes => ['Scope::Session', 'Scope::Read', 'Scope::Domain', 'Scope::Application', 'Scope::Userinfo'],
     :default_scope => 'userinfo',
     :scope_expirations => OpenShift::Controller::Configuration.parse_expiration(conf.get('AUTH_SCOPE_TIMEOUTS'), 1.day),
     :download_cartridges_enabled => conf.get_bool("DOWNLOAD_CARTRIDGES_ENABLED", "false"),    
