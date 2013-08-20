79570a9a6fe70ad6a7091d101228b18b91a77b1b
Jessica Forrester <jforrest@redhat.com>
2013-08-20 09:06:44 -0400

    Admin console welcome overview page

--- admin-console/app/assets/javascripts/admin_console/util.js
+++ admin-console/app/assets/javascripts/admin_console/util.js
@@ -17,3 +17,58 @@ function writeQueryParams(params) {
   }
   return query;
 }
+
+function tooltip_placement(tip, element) {
+  var $element, above, actualHeight, actualWidth, below, boundBottom, boundLeft, boundRight, boundTop, elementAbove, elementBelow, elementLeft, elementRight, isWithinBounds, left, pos, right;
+  isWithinBounds = function(elementPosition) {
+    return boundTop < elementPosition.top && boundLeft < elementPosition.left && boundRight > (elementPosition.left + actualWidth) && boundBottom > (elementPosition.top + actualHeight);
+  };
+  $element = $(element);
+  pos = $.extend({}, $element.offset(), {
+    width: element.offsetWidth,
+    height: element.offsetHeight
+  });
+  actualWidth = 283;
+  actualHeight = 117;
+  boundTop = $(document).scrollTop();
+  boundLeft = $(document).scrollLeft();
+  boundRight = boundLeft + $(window).width();
+  boundBottom = boundTop + $(window).height();
+  elementAbove = {
+    top: pos.top - actualHeight,
+    left: pos.left + pos.width / 2 - actualWidth / 2
+  };
+  elementBelow = {
+    top: pos.top + pos.height,
+    left: pos.left + pos.width / 2 - actualWidth / 2
+  };
+  elementLeft = {
+    top: pos.top + pos.height / 2 - actualHeight / 2,
+    left: pos.left - actualWidth
+  };
+  elementRight = {
+    top: pos.top + pos.height / 2 - actualHeight / 2,
+    left: pos.left + pos.width
+  };
+  above = isWithinBounds(elementAbove);
+  below = isWithinBounds(elementBelow);
+  left = isWithinBounds(elementLeft);
+  right = isWithinBounds(elementRight);
+  if (above) {
+    return "top";
+  } else {
+    if (below) {
+      return "bottom";
+    } else {
+      if (left) {
+        return "left";
+      } else {
+        if (right) {
+          return "right";
+        } else {
+          return "right";
+        }
+      }
+    }
+  }
+};
