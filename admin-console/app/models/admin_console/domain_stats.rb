85832a7a0ba4fe5ce91047e469c4dd8d31d0db5d
Jessica Forrester <jforrest@redhat.com>
2013-09-04 09:18:53 -0400

    Bug 1004160 - admin console stats graphs fail to load with arg error

--- admin-console/app/models/admin_console/domain_stats.rb
+++ admin-console/app/models/admin_console/domain_stats.rb
@@ -4,7 +4,7 @@ module AdminConsole
 
     def self.domains_per_user_binning
       return binning_from_mongo_aggregate([{"_id" => 0, "count" => CloudUser.count}]) unless Domain.count > 0
-      binning = binning_from_mongo_aggregate(collection.aggregate(
+      binning = binning_from_mongo_aggregate(collection.aggregate([
         {
           "$group" => {
             "_id" => "$owner_id",
@@ -21,7 +21,7 @@ module AdminConsole
             "_id" => "$domain_per_user_bin",
             "count" => { "$sum" => 1 }
           }
-        }))
+        }]))
       total_users_found = binning[:bins].inject(0) { |sum, bin| sum + bin[:count] }
       zero_bin = binning[:bins].find { |bin| bin[:bin] == 0 }
       zero_bin[:count] = CloudUser.count - total_users_found
