6c8ede85be10ecd9be23ad2c06b3f29a07301ec7
liggitt <jliggitt@redhat.com>
2013-09-25 09:37:00 -0400

    Render errors better on restart or alias delete errors

--- console/app/controllers/restarts_controller.rb
+++ console/app/controllers/restarts_controller.rb
@@ -11,8 +11,7 @@ class RestartsController < ConsoleController
       message = @application.messages.first || "The application '#{@application.name}' has been restarted"
       redirect_to @application, :flash => {:success => message.to_s}
     else
-      message = @application.errors[:base].presence || "The application '#{@application.name}' could not be restarted"
-      redirect_to @application, :flash => {:error => message}
+      render :show
     end
   end
 
