0ba3145c035bc0e052fce8290f619b6b8495db74
Rajat Chopra <rchopra@redhat.com>
2013-09-27 11:44:52 -0700

    fix bz1012374 - extra check for expose port for sparse-carts

--- controller/app/models/pending_ops/expose_port_op.rb
+++ controller/app/models/pending_ops/expose_port_op.rb
@@ -11,6 +11,7 @@ class ExposePortOp < PendingAppOp
   def addParallelExecuteJob(handle)
     gear = get_gear()
     component_instance = get_component_instance()
+    return if component_instance.is_sparse? and not gear.sparse_carts.include? component_instance._id
     job = gear.get_expose_port_job(component_instance)
     tag = { "expose-ports" => component_instance._id.to_s, "op_id" => self._id.to_s }
     RemoteJob.add_parallel_job(handle, tag, gear, job)
