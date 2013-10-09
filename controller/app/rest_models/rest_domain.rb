83a5b4150c47cbd3a8e4b65d9bd20b37af60a114
Ravi Sankar <rpenta@redhat.com>
2013-10-09 15:11:49 -0700

    Bug 1016171 - Fix domain allowed_gear_sizes  in oo-admin-ctl-user

--- controller/app/rest_models/rest_domain.rb
+++ controller/app/rest_models/rest_domain.rb
@@ -29,7 +29,7 @@ class RestDomain < OpenShift::Model
     self.suffix = Rails.application.config.openshift[:domain_suffix]
     self.creation_time = domain.created_at
     self.members = domain.members.map{ |m| RestMember.new(m, domain.owner_id == m._id, url, nolinks) }
-    self.allowed_gear_sizes = domain.allowed_gear_sizes
+    self.allowed_gear_sizes = (domain.allowed_gear_sizes & domain.owner.allowed_gear_sizes)
 
     if not domain.application_count.nil?
       @application_count = domain.application_count
