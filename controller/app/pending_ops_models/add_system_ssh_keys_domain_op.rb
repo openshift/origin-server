d03aa9237f78c34bec94b51f8693165726a27512
Lili Nader <lnader@redhat.com>
2014-01-08 12:03:29 -0800

    Allow multiple keys to added or removed at the same time

--- controller/app/pending_ops_models/add_system_ssh_keys_domain_op.rb
+++ controller/app/pending_ops_models/add_system_ssh_keys_domain_op.rb
@@ -4,6 +4,6 @@ class AddSystemSshKeysDomainOp < PendingDomainOps
 
   def execute
     ssh_keys = keys_attrs.map { |key_hash| SystemSshKey.new.to_obj(key_hash) } 
-    pending_apps.each { |app| app.add_ssh_keys(nil, ssh_keys, self) }
+    pending_apps.each { |app| app.add_ssh_keys(ssh_keys, self) }
   end
 end
