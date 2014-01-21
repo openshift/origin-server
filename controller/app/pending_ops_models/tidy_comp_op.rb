104b48defa53f1c4910398c67219481076391199
Abhishek Gupta <abhgupta@redhat.com>
2014-01-21 11:19:13 -0800

    Bug 1055878: calling tidy once per gear instead of per gear per cart

--- controller/app/pending_ops_models/tidy_comp_op.rb
+++ controller/app/pending_ops_models/tidy_comp_op.rb
@@ -1,6 +1,5 @@
 class TidyCompOp < PendingAppOp
 
-  field :comp_spec, type: Hash, default: {}
   field :gear_id, type: String
 
   def is_parallel_executable
@@ -10,8 +9,7 @@ class TidyCompOp < PendingAppOp
   def add_parallel_execute_job(handle)
     gear = get_gear()
     unless gear.removed
-      component_instance = get_component_instance()
-      job = gear.get_tidy_job(component_instance)
+      job = gear.get_tidy_job()
       tag = { "op_id" => self._id.to_s }
       RemoteJob.add_parallel_job(handle, tag, gear, job)
     end
