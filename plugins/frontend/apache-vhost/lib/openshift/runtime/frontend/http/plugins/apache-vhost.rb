6eaf1e1c8ceeca18e291a339e5baf66f8a2acded
Vincent Batts <vbatts@redhat.com>
2014-03-11 23:04:33 -0400

    vhost-plugin: remove keepalive=On for now

--- plugins/frontend/apache-vhost/lib/openshift/runtime/frontend/http/plugins/apache-vhost.rb
+++ plugins/frontend/apache-vhost/lib/openshift/runtime/frontend/http/plugins/apache-vhost.rb
@@ -184,7 +184,7 @@ module OpenShift
                     if gen_default_rule
                       tpath = path.empty? ? "/" : path
 
-                      f.puts("ProxyPass #{tpath} #{proxy_proto}://#{uri}/ keepalive=On")
+                      f.puts("ProxyPass #{tpath} #{proxy_proto}://#{uri}/")
 
                       if uri.empty?
                         turi = "127.0.0.1:80"
