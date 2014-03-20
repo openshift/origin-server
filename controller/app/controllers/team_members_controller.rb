32434073fbe14a5ff4d9db010d6c5c56e09b9b9d
Lili Nader <lnader@redhat.com>
2014-03-20 11:28:16 -0700

    moved global checking to before_filter

--- controller/app/controllers/team_members_controller.rb
+++ controller/app/controllers/team_members_controller.rb
@@ -1,26 +1,13 @@
 class TeamMembersController < MembersController
   include RestModelHelper
+  before_filter :check_global, :only => [:create, :destroy, :update]
   action_log_tag_resource :member
-  
-  def create
-    return render_error(:unprocessable_entity, "You cannot modify a teams that maps to an external group via the API.", 1) unless membership.maps_to.nil?
-    super
-  end
-  
-  def update
-    return render_error(:unprocessable_entity, "You cannot modify a teams that maps to an external group via the API.", 1) unless membership.maps_to.nil?
-    super
-  end
   
   def leave
     return render_error(:unprocessable_entity, "You cannot leave a global team.", 1) unless membership.maps_to.nil?
     super
   end
   
-  def destroy
-    return render_error(:unprocessable_entity, "You cannot modify a teams that maps to an external group via the API.", 1) unless membership.maps_to.nil?
-    super
-  end
 
   protected
 
@@ -34,6 +21,10 @@ class TeamMembersController < MembersController
 
     def allowed_member_types
       ["user"]
+    end
+    
+    def check_global
+      return render_error(:unprocessable_entity, "You cannot modify a teams that maps to an external group via the API.", 1) unless membership.maps_to.nil?
     end
 
 end
