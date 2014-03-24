c1ecb7ab8ed9465dbce47701d9fe835a5b2372e3
teddythetwig <teddythetwig@gmail.com>
2014-03-24 15:11:38 -0700

    bugfix

--- node-util/conf/watchman/plugins.d/metric_plugin.rb
+++ node-util/conf/watchman/plugins.d/metric_plugin.rb
@@ -90,7 +90,7 @@ module OpenShift
 
           # Timing this method is a waste of.... time
           def call_application_container_metrics
-            res,err, _ = Utils.oo_spawn("oo-admin-ctl-gear metricsall")
+            res,err, _ = Utils.oo_spawn("oo-admin-ctl-gears metricsall")
             Syslog.info(res)
           end
 
