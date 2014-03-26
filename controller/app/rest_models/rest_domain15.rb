6047033e9c71e5ddffd9e36d70b7d0c0d005034c
Lili Nader <lnader@redhat.com>
2014-03-26 13:09:21 -0700

    corrected UPDATE_MEMBER link for domain and team

--- controller/app/rest_models/rest_domain15.rb
+++ controller/app/rest_models/rest_domain15.rb
@@ -67,7 +67,7 @@ class RestDomain15 < OpenShift::Model
           [OptionalParam.new("type", "string", "The member's type. i.e. user or team", ["user", "team"], "user"),
           OptionalParam.new("id", "string", "Unique identifier of the member for the given member type (user or team ID)"),
           OptionalParam.new("login", "string", "The user's login attribute"),
-          OptionalParam.new("members", "Array", "An array of members to add with corresponding type and role. e.g. [{\"login\" => \"foo\", \"type\" => \"user\", \"role\" => \"view\"}, {\"id\" =>\"5326534e2046fde9d3000001\", \"type\"=> \"team\", \"role\" => \"edit\" }]")]
+          OptionalParam.new("members", "Array", "An array of members to add with corresponding type and role. e.g. {'members': [{'login': 'foo', 'type': 'user' 'role': 'view'}, {'id': '5326534e2046fde9d3000001', 'type': 'team', 'role': 'none'}]}")]
         ),
       }
     end
