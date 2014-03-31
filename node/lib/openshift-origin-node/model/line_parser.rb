607b1f16ec37781b0b9886c0d7890a550e569cd1
teddythetwig <teddythetwig@gmail.com>
2014-03-31 09:20:12 -0700

    added line parser

---
+++ node/lib/openshift-origin-node/model/line_parser.rb
@@ -0,0 +1,11 @@
+module OpenShift
+  module Runtime
+    class LineParser
+      def parse string
+        valid_form = ([a-zA-Z0-9]+=[a-zA-Z0-9])
+        matches = valid_form.match string
+        matches.to_a.join("\s")
+      end
+    end
+  end
+end
