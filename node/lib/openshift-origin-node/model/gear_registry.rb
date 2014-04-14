2fb2cf00da8316f766b3fdbf9c0304fb3f9ff3c0
Jhon Honce <jhonce@redhat.com>
2014-04-14 15:53:18 -0700

    Bug 1086427 - Do not use JSON.load() with untrusted JSON
    * see https://www.ruby-lang.org/en/news/2013/02/22/json-dos-cve-2013-0269/

--- node/lib/openshift-origin-node/model/gear_registry.rb
+++ node/lib/openshift-origin-node/model/gear_registry.rb
@@ -119,7 +119,8 @@ module OpenShift
 
         with_lock do
           File.open(@registry_file, "r") do |f|
-            raw_json = HashWithIndifferentAccess.new(JSON.load(f))
+            # JSON.load is not used to prevent class injection. BZ#1086427
+            raw_json = HashWithIndifferentAccess.new(JSON.parse(f.read))
             raw_json.each_pair do |type, entries|
               entries.each_pair do |uuid, entry|
                 add(entry.merge({type: type.to_sym, uuid: uuid}))
