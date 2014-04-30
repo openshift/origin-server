8c3566286a08dc8f2e80c4daab5110bc0b38f76d
Abhishek Gupta <abhgupta@redhat.com>
2014-04-30 16:33:34 -0700

    Passing a class instead of an instance for the gear placement plugin

--- plugin-examples/gear-placement/lib/gear_placement_engine.rb
+++ plugin-examples/gear-placement/lib/gear_placement_engine.rb
@@ -4,7 +4,7 @@ require 'rails'
 module OpenShift
   class GearPlacementEngine < ::Rails::Engine
     config.after_initialize do
-      OpenShift::ApplicationContainerProxy.node_selector_plugin = OpenShift::GearPlacementPlugin.new
+      OpenShift::ApplicationContainerProxy.node_selector_plugin = OpenShift::GearPlacementPlugin
     end
   end
 end
