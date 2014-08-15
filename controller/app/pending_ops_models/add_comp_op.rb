9e02f63c0044b3c76eb77c492672c08ac3436196
Abhishek Gupta <abhgupta@redhat.com>
2014-08-15 16:38:07 -0700

    Bug 1126826: cleaning up domain env vars and ssh keys on rollback

--- controller/app/pending_ops_models/add_comp_op.rb
+++ controller/app/pending_ops_models/add_comp_op.rb
@@ -19,6 +19,22 @@ class AddCompOp < PendingAppOp
       # since, in addition to a node operation, this also involves a mongo update for sparse carts
       result_io = gear.remove_component(get_component_instance)
     end
+
+    if (comp_inst = get_component_instance rescue nil)
+      # remove the ssh key for this component, if any
+      remove_ssh_keys = application.app_ssh_keys.find_by(component_id: comp_inst._id) rescue []
+      remove_ssh_keys = [remove_ssh_keys].flatten
+      if remove_ssh_keys.length > 0
+        keys_attrs = remove_ssh_keys.map{|k| k.attributes.dup}
+        op_group = UpdateAppConfigOpGroup.new(remove_keys_attrs: keys_attrs, user_agent: application.user_agent)
+        Application.where(_id: application._id).update_all({ "$push" => { pending_op_groups: op_group.as_document }, "$pullAll" => { app_ssh_keys: keys_attrs }})
+      end
+
+      # remove the ssh keys and environment variables from the domain, if any
+      application.domain.remove_system_ssh_keys(comp_inst._id)
+      application.domain.remove_env_variables(comp_inst._id)
+    end
+
     result_io
   end
 
--- controller/app/pending_ops_models/init_gear_op.rb
+++ controller/app/pending_ops_models/init_gear_op.rb
@@ -55,7 +55,23 @@ class InitGearOp < PendingAppOp
     # if the group_instance has no more gears, then delete it and its component instances
     if (instance = get_group_instance rescue nil) && instance.gears.blank?
       application.atomic_update do
-        instance.all_component_instances.each(&:delete)
+        instance.all_component_instances.each do |comp_inst|
+          comp_inst.delete
+
+          # remove the ssh key for this component, if any
+          remove_ssh_keys = application.app_ssh_keys.find_by(component_id: comp_inst._id) rescue []
+          remove_ssh_keys = [remove_ssh_keys].flatten
+          if remove_ssh_keys.length > 0
+            keys_attrs = remove_ssh_keys.map{|k| k.attributes.dup}
+            op_group = UpdateAppConfigOpGroup.new(remove_keys_attrs: keys_attrs, user_agent: application.user_agent)
+            Application.where(_id: application._id).update_all({ "$push" => { pending_op_groups: op_group.as_document }, "$pullAll" => { app_ssh_keys: keys_attrs }})
+          end
+
+          # remove the ssh keys and environment variables from the domain, if any
+          application.domain.remove_system_ssh_keys(comp_inst._id)
+          application.domain.remove_env_variables(comp_inst._id)
+        end
+
         instance.delete
       end rescue if_not_found($!)
     end
