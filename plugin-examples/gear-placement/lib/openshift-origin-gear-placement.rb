0d69cfe5e79c73f8ba5379989167a2d47cbf4355
Chris Alfonso <calfonso@redhat.com>
2014-08-18 09:49:47 -0600

    Remove the gear-placement plugin example since it's in openshift-extras
    openshift-extras is now the home of the gear-placement plugin

--- plugin-examples/gear-placement/lib/openshift-origin-gear-placement.rb
+++
@@ -1,7 +0,0 @@
-module OpenShift
-  module GearPlacementModule
-    require 'gear_placement_engine' if defined?(Rails) && Rails::VERSION::MAJOR == 3
-  end
-end
-
-require "openshift/gear_placement_plugin.rb"
