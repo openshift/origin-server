198873e2175cb727e2ba49f6fa377d37cf176432
Rajat Chopra <rchopra@redhat.com>
2014-08-21 11:30:40 -0700

    move env var guard for all http plugins and not just the vhost plugin

--- plugins/frontend/apache-vhost/lib/openshift/runtime/frontend/http/plugins/apache-vhost.rb
+++ plugins/frontend/apache-vhost/lib/openshift/runtime/frontend/http/plugins/apache-vhost.rb
@@ -424,7 +424,7 @@ module OpenShift
                   end
                 end
               end
-              ::OpenShift::Runtime::Frontend::Http::Plugins::reload_httpd unless ENV['APACHE_VHOST_DO_NOT_RELOAD']
+              ::OpenShift::Runtime::Frontend::Http::Plugins::reload_httpd
             end
 
             def with_lock_and_reload(&block)
