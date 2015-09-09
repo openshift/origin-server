openshift/origin-server

commit bb074e81c11d2eae94b3399939407782d1c38280

routing-daemon: Improve error reporting for F5
In the rest_request method, rescue exceptions from RestClient::Request
and check the response body for a description of the problem from F5 in
order to provide more descriptive error logging.

routing-daemon/lib/openshift/routing/models/f5-icontrol-rest.rb
@@ -37,10 +37,21 @@ module OpenShift
 
       expected_code = options.delete(:expected_code) || 200
 
-      RestClient::Request.execute(defaults.merge(options)).tap do |response|
-        unless response.code == expected_code
-          raise LBModelException.new "Expected HTTP #{expected_code} but got #{response.code} instead"
+      begin
+        RestClient::Request.execute(defaults.merge(options)).tap do |response|
+          unless response.code == expected_code
+            raise LBModelException.new "Expected HTTP #{expected_code} but got #{response.code} instead"
+          end
         end
+      rescue => e
+        msg = "got #{e.class} exception: #{e.to_s}"
+        begin
+          resp = JSON.parse e.response
+          m = resp['message']
+          msg += " (#{m})" unless m.empty?
+        rescue
+        end
+        raise LBModelException.new msg
       end
     end
 

commit e131a07c8568877001d74b504315c2d838ac0a58

routing-daemon: F5: Fix initialization
F5IControlRestLoadBalancerModel#rest_request: Take an option
:wrap_exceptions, default value true, to indicate whether rest_request
should rescue exceptions and wrap them in LBModelException objects.

F5IControlRestLoadBalancerModel#initialize: Specify wrap_exceptions: false
when getting the openshift_application_aliases policy so that initialize's
rescue RestClient::ResourceNotFound block will be executed as needed.

This commit fixes a defect introduced in commit
bb074e81c11d2eae94b3399939407782d1c38280 that prevented the routing-daemon
from creating the openshift_application_aliases policy if it did not
already exist during the routing-daemon's initialization.

routing-daemon/lib/openshift/routing/models/f5-icontrol-rest.rb
@@ -46,17 +46,22 @@ module OpenShift
         open_timeout: @open_timeout,
         user: @username,
         password: @password,
+        wrap_exceptions: true,
       }
 
       expected_code = options.delete(:expected_code) || 200
 
+      options.merge!(defaults) {|_,option,default| option}
+
       begin
-        RestClient::Request.execute(defaults.merge(options)).tap do |response|
+        RestClient::Request.execute(options).tap do |response|
           unless response.code == expected_code
             raise LBModelException.new "Expected HTTP #{expected_code} but got #{response.code} instead"
           end
         end
       rescue => e
+        raise unless options.wrap_exceptions
+
         msg = "got #{e.class} exception: #{e.message}"
         begin
           resp = JSON.parse e.response
@@ -64,6 +69,7 @@ module OpenShift
           msg += " (#{m})" unless m.empty?
         rescue
         end
+
         raise LBModelException.new msg
       end
     end
@@ -354,7 +360,8 @@ module OpenShift
       # the application-specific rules to and from the latter.
       policy = 'openshift_application_aliases'
       begin
-        get(url: "https://#{@host}/mgmt/tm/ltm/policy/#{policy}")
+        get(url: "https://#{@host}/mgmt/tm/ltm/policy/#{policy}",
+            wrap_exceptions: false)
       rescue RestClient::ResourceNotFound
         @logger.info "No #{policy} policy exists.  Creating..."
         post(url: "https://#{@host}/mgmt/tm/ltm/policy",